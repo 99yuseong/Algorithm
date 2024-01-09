de03c1c4778ce287b75ce99ec985ca2ad2e32733
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-01-09 18:54:52 +0900

    Update 히스토그램.swift

--- 백준/Platinum/1725. 히스토그램/히스토그램.swift
+++ 백준/Platinum/1725. 히스토그램/히스토그램.swift
@@ -8,23 +8,23 @@ for _ in 0..<n {
 }
 
 var stack: [Int] = []
-var answer: Int = 0
+var maxArea: Int = 0
 
 for i in 0..<n {
     while !stack.isEmpty && hist[stack.last!] > hist[i] {
-        let tmp = stack.popLast()!
+        let remove = stack.popLast()!
         let width = stack.isEmpty ? i : i - stack.last! - 1
-        answer = max(answer, width * hist[tmp])
-        
+        maxArea = max(maxArea, width * hist[remove])
     }
     stack.append(i)
 }
 
 while !stack.isEmpty {
-    let tmp = stack.popLast()!
+    let remove = stack.popLast()!
     let width = stack.isEmpty ? n : n - stack.last! - 1
-    answer = max(answer, width * hist[tmp])
+    maxArea = max(maxArea, width * hist[remove])
 }
 
-print(answer)
+print(maxArea)
+
 
