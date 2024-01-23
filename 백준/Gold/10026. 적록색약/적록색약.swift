9a42fd2e44ae9561ac0b8b7926b767c74bb41a71
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-01-23 21:13:49 +0900

    [Gold V] Title: 적록색약, Time: 16 ms, Memory: 79644 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var arr: [[Character]] = []
+var section1 = 0
+var section2 = 0
+
+let dx = [0, 0, -1, 1]
+let dy = [-1, 1, 0, 0]
+
+for _ in 0..<n {
+    arr.append(Array(readLine()!))
+}
+
+func bfs(_ isNormal: Bool) -> Int {
+    var section = 0
+    var queue: [[Int]] = []
+    var visited = Array(repeating: Array(repeating: false, count: n), count: n)
+    
+    for i in 0..<n {
+        for j in 0..<n {
+            if !visited[i][j] {
+                let sec = arr[i][j]
+                let isBlue = sec == "B"
+                section += 1
+                    
+                queue.append([i, j])
+                visited[i][j] = true
+                
+                while !queue.isEmpty {
+                    let cur = queue.removeFirst()
+                            
+                    for i in 0..<4 {
+                        let x = cur[0] + dx[i]
+                        let y = cur[1] + dy[i]
+                          
+                        if 0..<n ~= x && 0..<n ~= y && !visited[x][y] {
+                            switch arr[x][y] {
+                            case sec:
+                                break
+                            default:
+                                if !isNormal && sec != "B" && arr[x][y] != "B" {
+                                    break
+                                }
+                                continue
+                            }
+                            queue.append([x, y])
+                            visited[x][y] = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    return section
+}
+
+print("\(bfs(true)) \(bfs(false))")
