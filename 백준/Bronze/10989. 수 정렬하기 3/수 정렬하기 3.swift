ac4154fecddcbcae2444c63e2d3fb4e66e355bf3
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-04-29 20:24:37 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1216 ms, Memory: 263604 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)]
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45{ isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    /// String을 아스키코드로 받을 경우
+    @inline(__always) func readString() -> Int {
+        var str = 0
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += Int(now)
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let file = FileIO()
+
+let N = file.readInt()
+var counts = Array(repeating: 0, count: 10001)
+
+for _ in 0..<N {
+    counts[file.readInt() - 1] += 1
+}
+
+var result = ""
+for num in counts.enumerated() {
+    result += String(repeating: "\(num.offset + 1)\n", count: num.element)
+}
+print(result)
