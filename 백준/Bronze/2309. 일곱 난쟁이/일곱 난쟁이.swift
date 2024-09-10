b404ed7474ecc8c5fdfec564b660ee315cd9a226
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-10 18:23:03 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.swift
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.swift
@@ -1,31 +1,21 @@
 import Foundation
 
-var i = 0
-var heights: [Int] = []
-var first = 0
-var second = 0
+var nums: [Int] = []
 
-while i < 9 {
-    let input = Int(readLine()!)!
-    heights.append(input)
-    i += 1
+for _ in 0..<9 {
+    nums.append(Int(readLine()!)!)
 }
+nums.sort()
 
-let total = heights.reduce(0, +)
+let sum = nums.reduce(0, +)
 
-for j in 0..<8 {
-    for k in j+1..<9 {
-        if total - heights[j] - heights[k] == 100 {
-            first = k
-            second = j
-            break
+for i in 0..<9 {
+    for j in i..<9 {
+        if sum - nums[i] - nums[j] == 100 {
+            for k in 0..<9 {
+                if k != i && k != j { print(nums[k]) }
+            }
+            exit(0)
         }
     }
-}
-
-heights.remove(at: first)
-heights.remove(at: second)
-
-for height in heights.sorted() {
-    print(height)
 }
