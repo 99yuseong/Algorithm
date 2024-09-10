54b480001f5b7b25c3be042f3aa6491cc639382d
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-10 16:20:47 +0900

    [Bronze IV] Title: 세수정렬, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.swift
+++ 백준/Bronze/2752. 세수정렬/세수정렬.swift
@@ -1,5 +1,10 @@
 import Foundation
 
-var input = readLine()!.components(separatedBy: " ").map { Int($0)! }
-input.sort()
-print(input.map{ String($0) }.joined(separator: " "))
+print(
+    readLine()!
+      .components(separatedBy: " ")
+      .map { Int($0)! }
+      .sorted()
+      .map { String($0) }
+      .joined(separator: " ")
+)
