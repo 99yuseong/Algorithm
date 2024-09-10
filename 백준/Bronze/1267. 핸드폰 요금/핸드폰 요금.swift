30e4f0dd4ecc88d9afa2b80e19ab380c49d07631
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-10 18:52:37 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.swift
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.swift
@@ -1,20 +1,18 @@
 import Foundation
 
-let callNum = Int(readLine()!)
-let callTimes = readLine()!.components(separatedBy: " ").map { Int($0)! }
+let N = Int(readLine()!)!
+var y: Int = 0
+var m: Int = 0
 
-var yPrice: Int = 0
-var mPrice: Int = 0
+let times = readLine()!.split(separator: " ").map { Int($0)! }
 
-for call in callTimes {
-    yPrice += (Int(call / 30) + 1) * 10
-    mPrice += (Int(call / 60) + 1) * 15
+for time in times {
+    y += (Int(time / 30) + 1) * 10
+    m += (Int(time / 60) + 1) * 15
 }
 
-if yPrice == mPrice {
-    print("Y M \(yPrice)")
-} else if yPrice < mPrice {
-    print("Y \(yPrice)")
+if y == m {
+    print("Y M \(y)")
 } else {
-    print("M \(mPrice)")
+    print(y > m ? "M \(m)" : "Y \(y)")
 }
