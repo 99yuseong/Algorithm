d8140d2fd2b3e83242b8ac0342c5bff82bea1868
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-11 17:18:01 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.swift
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.swift
@@ -1,8 +1,6 @@
 import Foundation
 
-let n = Int(readLine()!)!
-
-for i in 1...n {
-    let line = String(repeating: " ", count: n - i) + String(repeating: "*", count: 2 * i - 1)
-    print(line)
+let N = Int(readLine()!)!
+for i in 1...N {
+    print(String(repeating: " ", count: N-i) + String(repeating: "*", count: 2 * i - 1))
 }
