5d91cea6c66d89a7c515e0f037ade1104f049d64
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-12 18:49:23 +0900

    [Silver V] Title: 방 번호, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.swift
+++ 백준/Silver/1475. 방 번호/방 번호.swift
@@ -1,14 +1,14 @@
 import Foundation
 
-var counts = [Int](repeating: 0, count: 10)
-
 let input = readLine()!
+var total = 1
+var arr = Array(repeating: 0, count: 10)
 
-for num in input {
-    counts[Int(String(num))!] += 1
+for char in input {
+    arr[Int(String(char))!] += 1
 }
 
-counts[9] = Int((counts[9] + counts[6] + 1) / 2)
-counts[6] = 0
-
-print(counts.max()!)
+let num = (arr[6] + arr[9] + 1) / 2
+arr[6] = num
+arr[9] = num
+print(arr.max()!)
