fb661be20112fb5246a8ec970973a6e36f09f981
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-14 00:37:54 +0900

    [Silver II] Title: 에디터, Time: 228 ms, Memory: 105924 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.swift
+++ 백준/Silver/1406. 에디터/에디터.swift
@@ -1,91 +1,14 @@
-//
-//import Foundation
-//
-//let string = readLine()!
-//let n = Int(readLine()!)!
-//var cursor = string.count
-//
-//let MX: Int = 100
-//var data: [String] = Array(repeating: "", count: MX)
-//var prev: [Int] = Array(repeating: -1, count: MX)
-//var next: [Int] = Array(repeating: -1, count: MX)
-//var unused: Int = 1
-//
-//func printData() {
-//    var result = ""
-//    var addr = 0
-//    while next[addr] != -1 {
-//        result += data[addr]
-//        addr = next[addr]
-//    }
-//    print(result)
-//}
-//
-//func insert(_ str: String, at addr: Int) {
-//    data[unused] = str
-//    prev[unused] = addr
-//    next[unused] = next[addr]
-//    if next[addr] != -1 {
-//        prev[next[addr]] = unused
-//    }
-//    next[addr] = unused
-//    unused += 1
-//}
-//
-//func remove(at addr: Int) {
-//    next[prev[addr]] = next[addr]
-//    if next[addr] != -1 {
-//        prev[next[addr]] = prev[addr]
-//    }
-//}
-//
-//next[0] = unused
-//for str in string {
-//    data[unused] = String(str)
-//    prev[unused] = unused - 1
-//    next[unused] = unused + 1
-//    unused += 1
-//}
-//
-//for _ in 0..<n {
-//    let command = readLine()!.components(separatedBy: " ")
-//    
-//    if command[0] == "L" {
-//        if prev[cursor] != -1 {
-//            cursor = prev[cursor]
-//        }
-//    }
-//    
-//    if command[0] == "D" {
-//        if next[cursor] != -1 {
-//            cursor = next[cursor]
-//        }
-//    }
-//    
-//    if command[0] == "B" {
-//        if prev[cursor] != -1 {
-//            remove(at: cursor)
-//            cursor = prev[cursor]
-//        }
-//    }
-//    
-//    if command[0] == "P" {
-//        insert(command[1], at: cursor)
-//        cursor = next[cursor]
-//    }
-//}
-//
-//printData()
-
 import Foundation
 
-var left = Array(readLine()!)
-var right: [Character] = []
-let n = Int(readLine()!)!
+let str = readLine()!
+let M = Int(readLine()!)!
+var left = Array(str)
+var right = Array<Character>()
 
-for _ in 0..<n {
-    let command = readLine()!
-    switch command {
+for _ in 0..<M {
+    let cmd = readLine()!
+    
+    switch cmd {
     case "L":
         if !left.isEmpty {
             right.append(left.removeLast())
@@ -99,7 +22,7 @@ for _ in 0..<n {
             left.removeLast()
         }
     default:
-        left.append(command.last!)
+        left.append(cmd.last!)
     }
 }
 
