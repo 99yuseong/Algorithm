fa9b9ee0d9cbbe62d8ae5a87538aa51b073ef1d8
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-16 14:23:56 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 52 ms, Memory: 87624 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.swift
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.swift
@@ -1,21 +1,22 @@
 import Foundation
 
-let n = Int(readLine()!)!
-var heights: [Int] = []
+let N = Int(readLine()!)!
+var tower: [[Int]] = []
 var stack: [Int] = []
-var answer: Int = 0
-
-for _ in 0..<n {
-    let height = Int(readLine()!)!
-    heights.append(height)
+var result: Int = 0
+// 0  1 2 3 4  5
+// 10 3 7 4 12 2
+for i in 0..<N {
+    tower.append([i, Int(readLine()!)!])
 }
-heights.append(Int.max)
+tower.append([N, Int.max])
 
-for i in (0...n) {
-    while !stack.isEmpty && heights[i] >= heights[stack.last!] {
-        answer += i - stack.popLast()! - 1
+// 10 같거나, 큰 수에서 Pop -> stack에 쌓이는 수는 자기보다 무조건 작다
+for i in 0...N {
+    while !stack.isEmpty && (tower[i][1] >= tower[stack.last!][1]) {
+        result += (tower[i][0] - tower[stack.removeLast()][0]) - 1
     }
-    stack.append(i)
+    stack.append(tower[i][0])
 }
 
-print(answer)
+print(result)
