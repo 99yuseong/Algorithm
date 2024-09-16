a9dd8a827e377b75553c248e03290d232acd9770
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-16 14:31:47 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 48 ms, Memory: 82608 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.swift
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.swift
@@ -1,22 +1,23 @@
 import Foundation
 
 let N = Int(readLine()!)!
-var tower: [[Int]] = []
+var tower: [Int] = []
 var stack: [Int] = []
 var result: Int = 0
-// 0  1 2 3 4  5
-// 10 3 7 4 12 2
+
 for i in 0..<N {
-    tower.append([i, Int(readLine()!)!])
+    tower.append(Int(readLine()!)!)
 }
-tower.append([N, Int.max])
+// 맨뒤에 제일 큰 빌딩을 두고 거기에서 다 계산이 되도록
+tower.append(Int.max)
 
-// 10 같거나, 큰 수에서 Pop -> stack에 쌓이는 수는 자기보다 무조건 작다
+// 같거나 큰 수에서 Pop -> stack에 쌓이는 수는 자기보다 무조건 작다
 for i in 0...N {
-    while !stack.isEmpty && (tower[i][1] >= tower[stack.last!][1]) {
-        result += (tower[i][0] - tower[stack.removeLast()][0]) - 1
+    while !stack.isEmpty && tower[i] >= tower[stack.last!] {
+        result += i - stack.removeLast() - 1
     }
-    stack.append(tower[i][0])
+    stack.append(i)
 }
 
+// 최종적으로 Stack에는 가장 큰 Int.max 빌딩만 남는다.
 print(result)
