9a5aa40576786513eb9beac0d2a9b0baf1f64093
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-16 14:40:05 +0900

    [Gold IV] Title: 오큰수, Time: 608 ms, Memory: 143624 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.swift
+++ 백준/Gold/17298. 오큰수/오큰수.swift
@@ -1,15 +1,21 @@
 import Foundation
 
-let n = Int(readLine()!)!
-let array = readLine()!.split(separator: " ").map { Int($0)! }
-var answer = [Int](repeating: -1, count: n)
-var stack: [Int] = []
+// 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수
+// == 오른쪽으로 가다가 만난 자기보다 큰 가장 첫 번째 수
+// >> Stack
+// 없을땐 -1
 
-for i in 0..<n {
-    while !stack.isEmpty && array[stack.last!] < array[i] {
-        answer[stack.removeLast()] = array[i]
+let A = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+var stack: [Int] = []
+var result = Array(repeating: -1, count: arr.count)
+
+for i in 0..<A {
+    while !stack.isEmpty && arr[i] > arr[stack.last!] {
+        result[stack.last!] = arr[i]
+        _ = stack.popLast()
     }
     stack.append(i)
 }
 
-print(answer.map { String($0) }.joined(separator: " "))
+print(result.map { String($0) }.joined(separator: " "))
