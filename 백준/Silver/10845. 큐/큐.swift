385632f81f6dd189792c67f6dbcfdb1e0d55513b
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-18 17:27:04 +0900

    [Silver IV] Title: 큐, Time: 24 ms, Memory: 79652 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.swift
+++ 백준/Silver/10845. 큐/큐.swift
@@ -1,45 +1,51 @@
 import Foundation
 
-let n = Int(readLine()!)!
-var inbox: [Int] = []
-var outbox: [Int] = []
+class Queue {
+    var inBox: [Int] = []
+    var outBox: [Int] = []
+    
+    var isEmpty: Bool { inBox.isEmpty && outBox.isEmpty }
+    var count: Int { inBox.count + outBox.count }
+    var front: Int? {
+        outBox.isEmpty ? inBox.first : outBox.last
+    }
+    var back: Int? {
+        inBox.isEmpty ? outBox.first : inBox.last
+    }
+    
+    func enqueue(_ value: Int) {
+        inBox.append(value)
+    }
+    
+    func dequeue() -> Int? {
+        if outBox.isEmpty {
+            outBox = inBox.reversed()
+            inBox.removeAll()
+        }
+        return outBox.popLast()
+    }
+}
 
-for _ in 0..<n {
+let N = Int(readLine()!)!
+var queue = Queue()
+
+for _ in 0..<N {
     let cmd = readLine()!.split(separator: " ")
     
-    switch cmd[0] {
-        case "pop":
-            if outbox.isEmpty && inbox.isEmpty {
-                print(-1)
-                break
-            }
-        
-            if outbox.isEmpty {
-                outbox = inbox.reversed()
-                inbox.removeAll()
-            }
-            print(outbox.popLast()!)
-        case "size":
-            print(inbox.count + outbox.count)
-        case "empty":
-            if inbox.isEmpty && outbox.isEmpty {
-                print(1)
-            } else {
-                print(0)
-            }
-        case "front":
-            if outbox.isEmpty {
-                print(inbox.first ?? -1)
-            } else {
-                print(outbox.last!)
-            }
-        case "back":
-            if inbox.isEmpty {
-                print(outbox.first ?? -1)
-            } else {
-                print(inbox.last!)
-            }
-        default: // push X
-            inbox.append(Int(cmd[1])!)
+    switch cmd.first! {
+    case "push":
+        queue.enqueue(Int(cmd.last!)!)
+    case "pop":
+        print(queue.dequeue() ?? -1)
+    case "size":
+        print(queue.count)
+    case "empty":
+        print(queue.isEmpty ? 1 : 0)
+    case "front":
+        print(queue.front ?? -1)
+    case "back":
+        print(queue.back ?? -1)
+    default:
+        break
     }
 }
