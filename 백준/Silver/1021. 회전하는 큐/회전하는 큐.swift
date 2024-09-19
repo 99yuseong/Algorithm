0dc0d584d613619185199ae6b64acb8224f0c6d7
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-19 15:32:54 +0900

    [Silver III] Title: 회전하는 큐, Time: 8 ms, Memory: 79520 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.swift
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.swift
@@ -1,25 +1,59 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let N = input[0]
-let n = input[1]
-let indexs = readLine()!.split(separator: " ").map { Int($0)! }
-
-var arr: [Int] = Array(1...N)
-var answer = 0
-var index = 0
-
-for i in 0..<n {
-    let targetIdx = arr.firstIndex(of: indexs[i])!
-    
-    if abs(targetIdx - index) * 2 > arr.count {
-        answer += arr.count - abs(targetIdx - index)
-    } else {
-        answer += abs(targetIdx - index)
+struct Deque {
+    var inBox: [Int] = []
+    var outBox: [Int] = []
+    var count: Int { inBox.count + outBox.count }
+    var isEmpty: Bool { inBox.isEmpty && outBox.isEmpty }
+    var front: Int? { inBox.isEmpty ? outBox.first : inBox.last }
+    var back: Int? { outBox.isEmpty ? inBox.first : outBox.last }
+    mutating func pushFront(_ value: Int) { inBox.append(value) }
+    mutating func pushBack(_ value: Int) { outBox.append(value) }
+    mutating func popFront() -> Int? {
+        if inBox.isEmpty {
+            inBox = outBox.reversed()
+            outBox.removeAll()
+        }
+        return inBox.popLast()
     }
-    
-    index = targetIdx % arr.count
-    arr.remove(at: targetIdx)
+    mutating func popBack() -> Int? {
+        if outBox.isEmpty {
+            outBox = inBox.reversed()
+            inBox.removeAll()
+        }
+        return outBox.popLast()
+    }
+    func firstIndex(of num: Int) -> Int? {
+        let newArr = inBox.reversed() + outBox
+        return newArr.firstIndex(of: num)
+    }
 }
 
-print(answer)
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let nums = readLine()!.split(separator: " ").map { Int($0)! }
+
+let N = input.first!
+let M = input.last!
+var deq = Deque()
+for i in 1...N {
+    deq.pushBack(i)
+}
+var result = 0
+
+for num in nums {
+    let targetIdx = deq.firstIndex(of: num)!
+    
+    if targetIdx * 2 <= deq.count { // 왼쪽
+        for _ in 0..<targetIdx {
+            deq.pushBack(deq.popFront()!)
+            result += 1
+        }
+    } else {
+        for _ in 0..<deq.count-targetIdx {
+            deq.pushFront(deq.popBack()!)
+            result += 1
+        }
+    }
+    _ = deq.popFront()
+}
+print(result)
