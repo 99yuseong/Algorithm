dd0b47a27a8206967d28ade2ccf1ca4e6d11d766
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-21 16:58:54 +0900

    [Gold III] Title: 불!, Time: 524 ms, Memory: 90776 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.swift
+++ 백준/Gold/4179. 불！/불！.swift
@@ -1,69 +1,77 @@
 import Foundation
 
+// 지훈, 불 동시 이동
 let input = readLine()!.split(separator: " ").map { Int($0)! }
-let (n, m) = (input[0], input[1])
+let mx = input.first!
+let my = input.last!
 
-var arr = Array(repeating: Array(repeating: -1, count: m), count: n)
-var visited = Array(repeating: Array(repeating: -1, count: m), count: n)
+let dx = [0, 1, 0, -1]
+let dy = [1, 0, -1, 0]
 
 var jPos: [Int] = []
+var map: [[Int]] = Array(repeating: Array(repeating: -1, count: my), count: mx)
+var visited: [[Int]] = Array(repeating: Array(repeating: -1, count: my), count: mx)
+
 var queue: [[Int]] = []
 
-for i in 0..<n {
-    let input = Array(readLine()!).map { String($0) }
+for i in 0..<mx {
+    let line = readLine()!.map { String($0) }
     
-    for (j, item) in input.enumerated() {
+    for (j, item) in line.enumerated() {
         switch item {
         case "#":
             break
         case "J":
-            jPos = [i, j]
-            arr[i][j] = 0
+            map[i][j] = 0
+            jPos = [i,j]
         case "F":
-            queue.append([i, j])
+            queue.append([i,j])
+        case ".":
+            map[i][j] = 0
         default:
-            arr[i][j] = 0
+            break
         }
     }
 }
 
-let dx = [0, 0, -1, 1]
-let dy = [-1, 1, 0, 0]
-
+// 불 Queue
 while !queue.isEmpty {
     let cur = queue.removeFirst()
-    let time = max(arr[cur[0]][cur[1]], 0)
+    let time = max(map[cur[0]][cur[1]], 0)
     
     for i in 0..<4 {
-        let x = cur[0] + dx[i]
-        let y = cur[1] + dy[i]
+        let x = cur.first! + dx[i]
+        let y = cur.last! + dy[i]
         
-        if 0..<n ~= x && 0..<m ~= y && arr[x][y] == 0 {
-            arr[x][y] = time + 1
-            queue.append([x, y])
+        if (0..<mx) ~= x && (0..<my) ~= y && map[x][y] == 0 {
+            queue.append([x,y])
+            map[x][y] = time + 1
         }
     }
 }
 
 queue.append(jPos)
 visited[jPos[0]][jPos[1]] = 0
-
+    
 while !queue.isEmpty {
     let cur = queue.removeFirst()
     let time = visited[cur[0]][cur[1]] + 1
     
     for i in 0..<4 {
-        let x = cur[0] + dx[i]
-        let y = cur[1] + dy[i]
-    
-        if x < 0 || x >= n || y < 0 || y >= m {
+        let x = cur.first! + dx[i]
+        let y = cur.last! + dy[i]
+        
+        if x < 0 || x > mx-1 || y < 0 || y > my-1 {
             print(time)
             exit(0)
         }
         
-        if 0..<n ~= x && 0..<m ~= y && visited[x][y] == -1 && (arr[x][y] == 0 || time < arr[x][y]) {
+        if (0..<mx) ~= x && (0..<my) ~= y
+            && visited[x][y] == -1
+            && (map[x][y] == 0 || time < map[x][y])
+        {
+            queue.append([x,y])
             visited[x][y] = time
-            queue.append([x, y])
         }
     }
 }
