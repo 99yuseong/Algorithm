da5c51a6dfe20fa0dfb3b7c4bad6517991c65967
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-22 18:26:53 +0900

    [Silver I] Title: 숨바꼭질, Time: 16 ms, Memory: 80976 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
@@ -1,51 +1,59 @@
+// N -> 십만
+// K -> 십만
+// 걸으면 앞뒤 +- 1
+// 순간이동 * 2
 import Foundation
 
-struct Q<T> {
+struct Q<T: Equatable> {
     var inbox: [T] = []
     var outbox: [T] = []
-    
-    var isEmpty: Bool {
-        return inbox.isEmpty && outbox.isEmpty
-    }
-    
-    mutating func append(_ x: T) {
-        inbox.append(x)
-    }
-    
-    mutating func removeFirst() -> T? {
+    var isEmpty: Bool { inbox.isEmpty && outbox.isEmpty }
+    mutating func enqueue(_ value: T) { inbox.append(value) }
+    mutating func dequeue() -> T? {
         if outbox.isEmpty {
             outbox = inbox.reversed()
             inbox.removeAll()
         }
-        
         return outbox.popLast()
     }
 }
 
 let input = readLine()!.split(separator: " ").map { Int($0)! }
-var subin = input[0]
-let sis = input[1]
+let N = input.first!
+let K = input.last!
 
-var queue = Q<[Int]>()
-var times = Array(repeating: 0, count: 100001)
-queue.append([subin, 0])
+if N == K {
+    print(0)
+    exit(0)
+} else if N > K {
+    print(N-K)
+    exit(0)
+}
+
+// N < K 인경우
+// 상황별 모든 케이스에서 가장 먼저 K에 도달하는 경우가 Win
+var map = Array(repeating: 0, count: 100001)
+var queue = Q<Int>()
+
+map[K] = -1
+queue.enqueue(N)
 
 while !queue.isEmpty {
-    let cur = queue.removeFirst()!
-    let pos = cur[0]
-    let time = cur[1]
+    let cur = queue.dequeue()!
+
+    let xs = [cur-1, cur+1, cur*2]
     
-    if pos == sis {
-        print(time)
-        exit(0)
-    }
-    
-    let moves = [pos - 1, pos + 1, pos * 2]
-    
-    for move in moves {
-        guard 0..<100001 ~= move else { continue }
-        guard times[move] == 0 else { continue }
-        times[move] = time + 1
-        queue.append([move, time + 1])
+    for x in xs {
+        guard (0..<100001) ~= x else { continue }
+        
+        if map[x] == -1 {
+            print(map[cur] + 1)
+            exit(0)
+        }
+        
+        if map[x] == 0 {
+            queue.enqueue(x)
+            map[x] = map[cur] + 1
+        }
     }
 }
