ebf01f79ed38cbd35af25eb735e98dc28937ef41
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-22 18:42:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 12 ms, Memory: 70568 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
@@ -1,9 +1,3 @@
-// N -> 십만
-// K -> 십만
-// 걸으면 앞뒤 +- 1
-// 순간이동 * 2
-import Foundation
-
 struct Q<T: Equatable> {
     var inbox: [T] = []
     var outbox: [T] = []
@@ -22,38 +16,37 @@ let input = readLine()!.split(separator: " ").map { Int($0)! }
 let N = input.first!
 let K = input.last!
 
-if N == K {
-    print(0)
-    exit(0)
-} else if N > K {
+switch N {
+case let x where x < K:
+    print(BFS()!)
+case let x where x > K:
     print(N-K)
-    exit(0)
+case let x where x == K:
+    print(0)
+default:
+    break
 }
 
-// N < K 인경우
-// 상황별 모든 케이스에서 가장 먼저 K에 도달하는 경우가 Win
-var map = Array(repeating: 0, count: 100001)
-var queue = Q<Int>()
+func BFS() -> Int? {
+    var map = Array(repeating: 0, count: 100001)
+    var queue = Q<Int>()
 
-map[K] = -1
-queue.enqueue(N)
+    map[K] = -1
+    queue.enqueue(N)
 
-while !queue.isEmpty {
-    let cur = queue.dequeue()!
-
-    let xs = [cur-1, cur+1, cur*2]
-    
-    for x in xs {
-        guard (0..<100001) ~= x else { continue }
+    while !queue.isEmpty {
+        let cur = queue.dequeue()!
         
-        if map[x] == -1 {
-            print(map[cur] + 1)
-            exit(0)
-        }
-        
-        if map[x] == 0 {
-            queue.enqueue(x)
-            map[x] = map[cur] + 1
+        for x in [cur-1, cur+1, cur*2] {
+            guard (0..<100001) ~= x else { continue }
+            
+            if map[x] == -1 { return map[cur] + 1 }
+            
+            if map[x] == 0 {
+                queue.enqueue(x)
+                map[x] = map[cur] + 1
+            }
         }
     }
+    return nil
 }
