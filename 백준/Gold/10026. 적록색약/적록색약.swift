121a6722ff76a5bea5b5ae178ce8cac1c197ce4c
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-24 01:04:04 +0900

    [Gold V] Title: 적록색약, Time: 12 ms, Memory: 69240 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.swift
+++ 백준/Gold/10026. 적록색약/적록색약.swift
@@ -1,55 +1,80 @@
-import Foundation
-
-let n = Int(readLine()!)!
-var arr: [[Character]] = []
-let dx = [0, 0, -1, 1]
-let dy = [-1, 1, 0, 0]
-
-for _ in 0..<n {
-    arr.append(Array(readLine()!))
-}
-
-func bfs(_ isNormal: Bool) -> Int {
-    var section = 0
-    var queue: [[Int]] = []
-    var visited = Array(repeating: Array(repeating: false, count: n), count: n)
-    
-    for i in 0..<n {
-        for j in 0..<n {
-            if !visited[i][j] {
-                let sec = arr[i][j]
-                section += 1
-                    
-                queue.append([i, j])
-                visited[i][j] = true
-                
-                while !queue.isEmpty {
-                    let cur = queue.removeFirst()
-                            
-                    for i in 0..<4 {
-                        let x = cur[0] + dx[i]
-                        let y = cur[1] + dy[i]
-                          
-                        if 0..<n ~= x && 0..<n ~= y && !visited[x][y] {
-                            switch arr[x][y] {
-                            case sec:
-                                break
-                            default:
-                                if !isNormal && sec != "B" && arr[x][y] != "B" {
-                                    break
-                                }
-                                continue
-                            }
-                            queue.append([x, y])
-                            visited[x][y] = true
-                        }
-                    }
-                }
+func BFS(_ color: String) {
+    while !queue.isEmpty {
+        let cur = queue.removeFirst()
+        
+        for i in 0..<4 {
+            let x = cur[0] + dx[i]
+            let y = cur[1] + dy[i]
+            
+            if (0..<N) ~= x
+                && (0..<N) ~= y
+                && visited[x][y] == false
+                && map[x][y] == color
+            {
+                queue.append([x,y])
+                visited[x][y] = true
+                map[x][y] = color == "B" ? "B" : "R"
             }
         }
     }
-    
-    return section
 }
 
-print("\(bfs(true)) \(bfs(false))")
+func BFS_True(_ color: String) {
+    while !queue.isEmpty {
+        let cur = queue.removeFirst()
+        
+        for i in 0..<4 {
+            let x = cur[0] + dx[i]
+            let y = cur[1] + dy[i]
+            
+            if (0..<N) ~= x
+                && (0..<N) ~= y
+                && map[x][y] == color {
+                queue.append([x,y])
+                map[x][y] = ""
+            }
+        }
+    }
+}
+
+let N = Int(readLine()!)!
+var map = Array(repeating: Array(repeating: "", count: N), count: N)
+var visited = Array(repeating: Array(repeating: false, count: N), count: N)
+
+for i in 0..<N {
+    let line = readLine()!.map { String($0) }
+    map[i] = line
+}
+
+var queue: [[Int]] = []
+let dx = [0, 1, 0, -1]
+let dy = [1, 0, -1, 0]
+var isTrue = 0
+var isFalse = 0
+
+for i in 0..<N {
+    for j in 0..<N {
+        if !visited[i][j] {
+            let color = map[i][j]
+            queue.append([i,j])
+            visited[i][j] = true
+            map[i][j] = color == "B" ? "B" : "R"
+            BFS(color)
+            isFalse += 1
+        }
+    }
+}
+
+for i in 0..<N {
+    for j in 0..<N {
+        if map[i][j] != "" {
+            let color = map[i][j]
+            queue.append([i,j])
+            map[i][j] = ""
+            BFS_True(color)
+            isTrue += 1
+        }
+    }
+}
+
+print("\(isFalse) \(isTrue)")
