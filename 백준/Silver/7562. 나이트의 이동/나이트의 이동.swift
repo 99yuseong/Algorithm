793587eac871e57df425691d3877f8da635fbd2c
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-24 20:03:52 +0900

    [Silver I] Title: 나이트의 이동, Time: 188 ms, Memory: 69908 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.swift
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.swift
@@ -1,55 +1,32 @@
-import Foundation
+let N = Int(readLine()!)!
 
-struct Q<T> {
-    var inbox: [T] = []
-    var outbox: [T] = []
-    var isEmpty: Bool {
-        return inbox.isEmpty && outbox.isEmpty
-    }
-    
-    mutating func append(_ x: T) {
-        inbox.append(x)
-    }
-    
-    mutating func popFirst() -> T? {
-        if outbox.isEmpty {
-            outbox = inbox.reversed()
-            inbox.removeAll()
-        }
-        
-        return outbox.popLast()
-    }
-}
-
-let T = Int(readLine()!)!
-let dx = [-2, -2, -1, -1, 1, 1, 2, 2]
-let dy = [-1, 1, -2, 2, -2, 2, -1, 1]
-
-for _ in 0..<T {
+out: for _ in 0..<N {
     let I = Int(readLine()!)!
-    var times = Array(repeating: Array(repeating: -1, count: I), count: I)
-    let cur = readLine()!.split(separator: " ").map { Int($0)! }
-    let target = readLine()!.split(separator: " ").map { Int($0)! }
+    let fromPos = readLine()!.split(separator: " ").map { Int($0)! }
+    let toPos = readLine()!.split(separator: " ").map { Int($0)! }
     
-    var queue = Q<[Int]>()
-    queue.append(cur)
-    times[cur[0]][cur[1]] = 0
+    let move = [[1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,-1], [-2,1], [-1,2]]
+    var arr = Array(repeating: Array(repeating: 0, count: I), count: I)
+    var queue: [[Int]] = []
+    
+    queue.append(fromPos)
+    arr[fromPos[0]][fromPos[1]] = 1
     
     while !queue.isEmpty {
-        let cur = queue.popFirst()!
+        let cur = queue.removeFirst()
+        
+        if toPos == [cur[0],cur[1]] {
+            print(arr[cur[0]][cur[1]]-1)
+            continue out
+        }
         
         for i in 0..<8 {
-            let x = cur[0] + dx[i]
-            let y = cur[1] + dy[i]
+            let x = cur[0] + move[i][0]
+            let y = cur[1] + move[i][1]
             
-            if cur == target {
-                print(times[cur[0]][cur[1]])
-                break
-            }
-            
-            if 0..<I ~= x && 0..<I ~= y && times[x][y] == -1 {
-                queue.append([x, y])
-                times[x][y] = times[cur[0]][cur[1]] + 1
+            if 0..<I ~= x && 0..<I ~= y && arr[x][y] == 0 {
+                queue.append([x,y])
+                arr[x][y] = arr[cur[0]][cur[1]] + 1
             }
         }
     }
