a234d0681c1af771f0423fbf996b58b51622ec97
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-24 20:06:11 +0900

    [Silver I] Title: 나이트의 이동, Time: 152 ms, Memory: 70004 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.swift
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.swift
@@ -1,3 +1,17 @@
+struct Q<T: Equatable> {
+    var inbox: [T] = []
+    var outbox: [T] = []
+    var isEmpty: Bool { inbox.isEmpty && outbox.isEmpty }
+    mutating func enqueue(_ value: T) { inbox.append(value) }
+    mutating func dequeue()-> T? {
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        return outbox.popLast()
+    }
+}
+
 let N = Int(readLine()!)!
 
 out: for _ in 0..<N {
@@ -7,13 +21,13 @@ out: for _ in 0..<N {
     
     let move = [[1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,-1], [-2,1], [-1,2]]
     var arr = Array(repeating: Array(repeating: 0, count: I), count: I)
-    var queue: [[Int]] = []
+    var queue = Q<[Int]>()
     
-    queue.append(fromPos)
+    queue.enqueue(fromPos)
     arr[fromPos[0]][fromPos[1]] = 1
     
     while !queue.isEmpty {
-        let cur = queue.removeFirst()
+        let cur = queue.dequeue()!
         
         if toPos == [cur[0],cur[1]] {
             print(arr[cur[0]][cur[1]]-1)
@@ -25,7 +39,7 @@ out: for _ in 0..<N {
             let y = cur[1] + move[i][1]
             
             if 0..<I ~= x && 0..<I ~= y && arr[x][y] == 0 {
-                queue.append([x,y])
+                queue.enqueue([x,y])
                 arr[x][y] = arr[cur[0]][cur[1]] + 1
             }
         }
