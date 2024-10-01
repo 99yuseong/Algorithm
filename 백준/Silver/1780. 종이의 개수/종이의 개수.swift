41d984008502d8a78c9a29918079517923258165
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-10-01 18:20:28 +0900

    [Silver II] Title: 종이의 개수, Time: 1068 ms, Memory: 106868 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.swift
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.swift
@@ -1,48 +1,37 @@
 let N = Int(readLine()!)!
 var paper: [[Int]] = []
+var answer: [Int] = [0,0,0]
 
 for _ in 0..<N {
     let line = readLine()!.split(separator: " ").map { Int($0)! }
     paper.append(line)
 }
 
-// 9등분해서 값을 리턴
-func sol(_ x: Int, _ y: Int, _ n: Int) -> [Int] {
-    if n == 1 {
-        switch paper[x][y] {
-        case -1: return [1,0,0]
-        case 0: return [0,1,0]
-        default: return [0,0,1]
-        }
-    }
-        
+func check(_ x: Int, _ y: Int, _ n: Int) -> Bool {
     for i in x..<x+n {
         for j in y..<y+n {
             if paper[i][j] != paper[x][y] {
-                return [
-                    sol(x + n * 0/3, y + n * 0/3, n/3),
-                    sol(x + n * 0/3, y + n * 1/3, n/3),
-                    sol(x + n * 0/3, y + n * 2/3, n/3),
-                    sol(x + n * 1/3, y + n * 0/3, n/3),
-                    sol(x + n * 1/3, y + n * 1/3, n/3),
-                    sol(x + n * 1/3, y + n * 2/3, n/3),
-                    sol(x + n * 2/3, y + n * 0/3, n/3),
-                    sol(x + n * 2/3, y + n * 1/3, n/3),
-                    sol(x + n * 2/3, y + n * 2/3, n/3),
-                ].reduce([0,0,0]) {
-                    [$0[0] + $1[0], $0[1] + $1[1], $0[2] + $1[2]]
-                }
+                return false
             }
         }
     }
+    return true
+}
+
+func sol(_ x: Int, _ y: Int, _ n: Int) {
+    if check(x,y,n) {
+        answer[paper[x][y]+1] += 1
+        return
+    }
     
-    switch paper[x][y] {
-    case -1: return [1,0,0]
-    case 0: return [0,1,0]
-    default: return [0,0,1]
+    for i in 0..<3 {
+        for j in 0..<3 {
+            sol(x + (i * n) / 3 , y + (j * n) / 3, n / 3)
+        }
     }
 }
 
-for cnt in sol(0, 0, N) {
-    print(cnt)
-}
+sol(0, 0, N)
+print(answer[0])
+print(answer[1])
+print(answer[2])
