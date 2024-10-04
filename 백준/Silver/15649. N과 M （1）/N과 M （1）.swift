358a9ee751127b7243f845fbcbed6a78ed78477a
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-10-04 22:17:18 +0900

    [Silver III] Title: N과 M (1), Time: 84 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -1,18 +1,19 @@
 let input = readLine()!.split(separator: " ").map { Int($0)! }
-let N = input[0] // >= M
+let N = input[0]
 let M = input[1]
 
-var arr = Array(repeating: 0, count: 10)
-var isUsed = Array(repeating: false, count: 10)
+var arr = Array(repeating: 0, count: M)
+var isUsed = Array(repeating: false, count: N)
 
 func sol(_ k: Int) {
     if k == M {
-        print(arr[0..<M].map { String($0) }.joined(separator: " "))
+        print(arr.map { String($0) }.joined(separator: " "))
+        return
     }
     
-    for i in 1...N {
+    for i in 0..<N {
         if !isUsed[i] {
-            arr[k] = i
+            arr[k] = i+1
             isUsed[i] = true
             sol(k+1)
             isUsed[i] = false
