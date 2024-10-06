19f8091442c4a35a82e7619a015a40d9567c3120
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-10-06 20:38:23 +0900

    [Silver III] Title: N과 M (3), Time: 736 ms, Memory: 100600 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.swift
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.swift
@@ -3,7 +3,6 @@ let N = input[0]
 let M = input[1]
 
 var arr = Array(repeating: 0, count: M)
-var isUsed = Array(repeating: 0, count: N)
 var answer = ""
 
 func sol(_ k: Int) {
@@ -13,12 +12,8 @@ func sol(_ k: Int) {
     }
     
     for i in 0..<N {
-        if isUsed[i] <= M {
-            arr[k] = i+1
-            isUsed[i] += 1
-            sol(k+1)
-            isUsed[i] -= 1
-        }
+        arr[k] = i+1
+        sol(k+1)
     }
 }
 
