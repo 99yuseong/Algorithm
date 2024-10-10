9f7a13ecc13b6796650df63d77d6a99f670e56ba
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-10-10 21:09:25 +0900

    [Silver III] Title: N과 M (5), Time: 100 ms, Memory: 69112 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.swift
@@ -0,0 +1,24 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let N = input[0]
+let M = input[1]
+
+var arr: [Int] = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+var box = Array(repeating: 0, count: M)
+var isUsed = Array(repeating: false, count: N)
+
+func solution(_ k: Int) {
+    if k == M {
+        print(box.map { String($0) }.joined(separator: " "))
+        return
+    }
+    
+    for i in 0..<N {
+        if !isUsed[i] {
+            isUsed[i] = true
+            box[k] = arr[i]
+            solution(k+1)
+            isUsed[i] = false
+        }
+    }
+}
+solution(0)
