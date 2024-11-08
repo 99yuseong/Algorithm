393db09d270964636affa5242ec65e08fb9c2cdd
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-11-08 22:59:46 +0900

    [Silver III] Title: N과 M (6), Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.swift
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.swift
@@ -1,24 +1,22 @@
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let N = input[0]
 let M = input[1]
+var arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+var ans = Array(repeating: 0, count: M)
 
-var arr: [Int] = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
-var box = Array(repeating: 0, count: M)
-var isUsed = Array(repeating: false, count: N)
-
-func solution(_ k: Int) {
+func box(_ k: Int) { // 현재까지 k개의 수를 택함
     if k == M {
-        print(box.map { String($0) }.joined(separator: " "))
+        print(ans.map { String(arr[$0]) }.joined(separator: " "))
         return
     }
     
-    for i in k..<N {
-        if !isUsed[i] && (k == 0 || arr[i] > box[k-1]) {
-            isUsed[i] = true
-            box[k] = arr[i]
-            solution(k+1)
-            isUsed[i] = false
-        }
+    var start = 0
+    if k != 0 { start = ans[k-1] + 1 }
+    guard start < N else { return }
+    
+    for i in start..<N {
+        ans[k] = i
+        box(k+1)
     }
 }
-solution(0)
+box(0)
