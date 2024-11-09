9eef8f957b5a5a096a9f0e9d5b6ab8cf51f0a395
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-11-09 13:02:06 +0900

    [Silver III] Title: N과 M (8), Time: 16 ms, Memory: 69612 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.swift
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.swift
@@ -2,19 +2,21 @@ let input = readLine()!.split(separator: " ").map { Int($0)! }
 let N = input[0]
 let M = input[1]
 var arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
-var ans = Array(repeating: 0, count: M)
+var ans = Array(repeating: "", count: M)
+var result = ""
 
 func box(_ k: Int) { // 현재까지 k개의 수를 택함
     if k == M {
-        print(ans.map { String(arr[$0]) }.joined(separator: " "))
+        result += ans.joined(separator: " ") + "\n"
         return
     }
     
-    let start = k == 0 ? 0 : ans[k-1]
-    
-    for i in start..<N {
-        ans[k] = i
-        box(k+1)
+    for i in 0..<N {
+        if k == 0 || Int(ans[k-1])! <= arr[i] {
+            ans[k] = String(arr[i])
+            box(k+1)
+        }
     }
 }
 box(0)
+print(result)
