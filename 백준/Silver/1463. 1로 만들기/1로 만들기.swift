3586e9b5b51dd7f352bbf8d6031ecd7b5be96bc4
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-11-25 16:09:30 +0900

    [Silver III] Title: 1로 만들기, Time: 8 ms, Memory: 70084 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.swift
@@ -0,0 +1,55 @@
+// 정부 X에 사용할 수 있는 연산
+
+// X가 3으로 나누어 떨어지면, 3으로 나눈다.
+// X가 2로 나누어 떨어지면, 2로 나눈다.
+// 1을 뺀다.
+
+// 연산 3개를 사용해서 1
+// N -> 1로 만드는 연산의 최솟값
+// 1 <= N <= 10^6 (백만)
+
+let N = Int(readLine()!)!
+var queue = Q<[Int]>()
+var visited = Array(repeating: false, count: N)
+
+struct Q<T> {
+    var inbox: [T] = []
+    var outbox: [T] = []
+    var isEmpty: Bool { inbox.isEmpty && outbox.isEmpty }
+    mutating func append(_ item: T) {
+        inbox.append(item)
+    }
+    mutating func removeFirst() -> T {
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        
+        return outbox.removeLast()
+    }
+}
+
+queue.append([N, 0])
+while !queue.isEmpty {
+    let cur = queue.removeFirst()
+    
+    if cur[0] == 1 {
+        print(cur[1])
+        break
+    }
+    
+    if !visited[cur[0]/3] && cur[0] % 3 == 0 {
+        queue.append([cur[0]/3, cur[1]+1])
+        visited[cur[0]/3] = true
+    }
+    
+    if !visited[cur[0]/2] && cur[0] % 2 == 0 {
+        queue.append([cur[0]/2, cur[1]+1])
+        visited[cur[0]/2] = true
+    }
+    
+    if !visited[cur[0]-1] {
+        queue.append([cur[0]-1, cur[1]+1])
+        visited[cur[0]-1] = true
+    }
+}
