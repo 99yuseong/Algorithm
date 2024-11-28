d9131da3731875e3e7511e366a39b38a61117eaa
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-11-28 17:39:00 +0900

    [Silver II] Title: 연속합, Time: 28 ms, Memory: 76116 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.swift
+++ 백준/Silver/1912. 연속합/연속합.swift
@@ -7,20 +7,17 @@
 // N  개수  1~100000(십만)
 // 수는 -1000 ~ 1000 -1억 ~ 1억
 
-// D[N]: N까지의 부분합 중 최대값 N 포함 / N 제외
-// D[1] = arr[1]
-// D[2] = max(D[N-1] + A[N], A[N])
-
-import Foundation
+// D[N]: "N번째 수를 포함"한 연속합의 최댓값
+// D[N] = max(D[N-1]+A[N], A[N])
+// 즉, N-1번째 수를 포함한 최댓값에 A[N]을 추가로 포함하는 게 더 큰가 
+// (이전 연속합이 음수인 경우)
+// 아니면, A[N]자체로 혼자 시작하는게 더 큰가
 
 let N = Int(readLine()!)!
 var A = [0] + readLine()!.split(separator: " ").map { Int($0)! }
 var D = Array(repeating: -1001, count: N+1)
 
-if N == 1 { print(A[1]); exit(0) }
-D[1] = A[1]
-
-for i in 2...N {
+for i in 1...N {
     D[i] = max(D[i-1] + A[i], A[i])
 }
 print(D.max()!)
