389bb2b22c9e178c12325c6bdde95f4a50552b72
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-11-30 12:28:37 +0900

    [Gold V] Title: 퇴사 2, Time: 1468 ms, Memory: 80824 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.swift
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.swift
@@ -7,24 +7,21 @@
 // P: 1~1000 -> Int
 
 let N = Int(readLine()!)!
-var T: [Int] = [0]
-var P: [Int] = [0]
 var D = Array(repeating: 0, count: N+1)
-
-for _ in 0..<N {
-    let TP = readLine()!.split(separator: " ").map { Int($0)! }
-    T.append(TP[0])
-    P.append(TP[1])
-}
 
 // D[i]: i일 작업을 했을 때, 최대 이익
 // D[i] = max(D[j] + P[i], D[i]) if j + T[j] - 1 < i
 
 for i in 1...N {
+    let TP = readLine()!.split(separator: " ").map { Int($0)! }
+    let T = TP[0]
+    let P = TP[1]
+    
     D[i] = max(D[i], D[i-1])
     
-    if i+T[i]-1 <= N {
-        D[i+T[i]-1] = max(D[i+T[i]-1], D[i-1]+P[i])
+    if i+T-1 <= N {
+        D[i+T-1] = max(D[i+T-1], D[i-1]+P)
     }
 }
+
 print(D.last!)
