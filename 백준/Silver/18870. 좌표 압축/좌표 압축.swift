aab14cd73f985acb3e85e550b37a83203b392ffd
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-01-02 10:25:25 +0900

    [Silver II] Title: 좌표 압축, Time: 1032 ms, Memory: 144928 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.swift
@@ -0,0 +1,37 @@
+// 2초
+// 512MB
+
+// N개의 좌표
+// Xi좌표 압축 -> Xi' = Xi보다 작은 다른 좌표 Xj의 개수
+
+// N: 좌표 1~100만개
+// Xi: -10억 ~ 10억
+
+// 우선 배열 A에 값을 정렬
+// func upperBound(_ target: Int) -> Int 
+// target과 처음 같은 수의 인덱스(없다면) target보다 큰 첫 수의 인덱스
+// 이분탐색을 이용
+
+// 중복 제거: O(2N) -> 200백만
+// 배열 정렬: O(NlgN) -> 600백만
+// N개를 이분탐색: O(NlgN) -> 600백만
+
+func upperBound(_ target: Int) -> Int {
+    var min = 0
+    var max = sortedA.count
+    
+    while min < max {
+        let mid = (min + max) / 2
+
+        if sortedA[mid] < target { min = mid + 1 }
+        else { max = mid }
+    }
+    
+    return min
+}
+
+let N = Int(readLine()!)!
+let A = readLine()!.split(separator: " ").map { Int($0)! }
+var sortedA = Array(Set(A)).sorted()
+
+print(A.map { String(upperBound($0)) }.joined(separator: " "))
