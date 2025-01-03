502cd066e912999fe4122f25df3b3351c0c770f6
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-01-03 14:09:23 +0900

    [Silver IV] Title: 차집합, Time: 660 ms, Memory: 114912 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.swift
+++ 백준/Silver/1822. 차집합/차집합.swift
@@ -20,14 +20,49 @@ let NaNb = readLine()!.split(separator: " ").map { Int($0)! }
 let Na = NaNb[0]
 let Nb = NaNb[1]
 
-var setA = Set(readLine()!.split(separator: " ").map { Int($0)! })
-var setB = Set(readLine()!.split(separator: " ").map { Int($0)! })
+//var setA = Set(readLine()!.split(separator: " ").map { Int($0)! })
+//var setB = Set(readLine()!.split(separator: " ").map { Int($0)! })
+//
+//setA.subtract(setB)
+//
+//if setA.isEmpty {
+//    print(0)
+//} else {
+//    print(setA.count)
+//    print(Array(setA).sorted().map { String($0) }.joined(separator: " "))
+//}
 
-setA.subtract(setB)
+var A = readLine()!.split(separator: " ").map { Int($0)! }
+var B = readLine()!.split(separator: " ").map { Int($0)! }
+var newA: [Int] = []
 
-if setA.isEmpty {
+A.sort() // O(NlgN)
+B.sort() // O(NlgN)
+
+// O(NlgN)
+for a in A {
+    if !binarySearch(a) { newA.append(a) }
+}
+
+if newA.isEmpty {
     print(0)
 } else {
-    print(setA.count)
-    print(Array(setA).sorted().map { String($0) }.joined(separator: " "))
+    print(newA.count)
+    print(newA.map { String($0) }.joined(separator: " "))
+}
+
+func binarySearch(_ x: Int) -> Bool {
+    var min = 0
+    var max = B.count-1
+    
+    while min <= max {
+        let mid = (min + max) / 2
+        
+        if B[mid] == x { return true }
+        
+        if B[mid] < x { min = mid + 1 }
+        else { max = mid - 1 }
+    }
+    
+    return false
 }
