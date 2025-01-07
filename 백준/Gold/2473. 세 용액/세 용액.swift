0792b225c6734cfad0ae5db93d9b95963273d63a
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-01-07 15:51:28 +0900

    [Gold III] Title: 세 용액, Time: 664 ms, Memory: 69632 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.swift
+++ 백준/Gold/2473. 세 용액/세 용액.swift
@@ -1,36 +1,45 @@
+// 1초
+// 256MB
+
+// 산성 1~10억
+// 알칼리성 -10억~-1
+
+// 세가지 용액을 혼합해서 0에 가장 가까운 용액
+// A[x] + A[y] + A[z] ~~ 0에 가까운 용액...
+
+// N: 3~5000개의 용액
+// 오름차순으로 출력
+
 let N = Int(readLine()!)!
 var A = readLine()!.split(separator: " ").map { Int($0)! }
 
-var itemX = 1000000001
-var itemY = 1000000001
-var itemZ = 1000000001
-
+var result = Array(repeating: 1000000001, count: 3)
 A.sort()
+
 
 for x in 0..<N {
     for y in (x+1)..<N {
         let lb = lowerBound(st: y+1, x: -A[x]-A[y])
         
-        for k in -2...2 {
-            let idx = lb + k
-            if 0..<N ~= idx && x != idx && y != idx {
-                let currentSum = abs(A[x] + A[y] + A[idx])
-                let bestSum = abs(itemX + itemY + itemZ)
-                if currentSum < bestSum {
-                    itemX = A[x]
-                    itemY = A[y]
-                    itemZ = A[idx]
-                }
+        for i in -1...1 {
+            let idx = lb + i
+            
+            guard 0..<N ~= idx else { continue }
+            guard x != idx && y != idx else { continue }
+            
+            if abs(result[0] + result[1] + result[2]) > abs(A[x] + A[y] + A[idx]) {
+                result[0] = A[x]
+                result[1] = A[y]
+                result[2] = A[idx]
             }
         }
     }
 }
-
-let result = [itemX, itemY, itemZ].sorted()
-print(result.map(String.init).joined(separator: " "))
+result.sort()
+print(result.map { String($0) }.joined(separator: " "))
 
 func lowerBound(st: Int, x: Int) -> Int {
-    var min = st
+    var min = 0
     var max = N
     
     while min < max {
@@ -39,5 +48,6 @@ func lowerBound(st: Int, x: Int) -> Int {
         if A[mid] < x { min = mid + 1 }
         else { max = mid }
     }
+    
     return min
 }
