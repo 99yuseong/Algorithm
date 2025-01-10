16f4e3348837ad8478036b846ffc8d13d015d691
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-01-10 20:18:59 +0900

    [Gold III] Title: 소수의 연속합, Time: 80 ms, Memory: 89664 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+// 연속된 소수의 합으로 나타낼 수 있는 경우의 수
+let N = Int(readLine()!)!
+
+// 소수 배열 -> 부분 합으로 N이 될 수 있는가?
+if N == 1 {
+    print(0)
+    exit(0)
+}
+
+// 1. 소수 배열 -> 에라토스테네스의 체
+var isPrime = Array(repeating: true, count: N+1)
+isPrime[0] = false
+isPrime[1] = false
+
+for i in 0..<isPrime.count {
+    if !isPrime[i] { continue }
+    
+    // i의 배수를 모두 false처리
+    var j = i * 2
+    while j <= N {
+        isPrime[j] = false
+        j += i
+    }
+}
+
+var A = (2...N).filter { isPrime[$0] } // N과 같거나 작은 소수의 배열
+
+// 2. 소수 배열의 부분합으로 N이 되는 경우의 수 찾기
+var en = 0
+var tot = A[0]
+var cnt = 0
+
+for st in 0..<A.count {
+    while tot < N && en < A.count {
+        en += 1
+        if en != A.count { tot += A[en] }
+    }
+    
+    if en == A.count { break }
+    
+    if tot == N { cnt += 1 }
+    tot -= A[st]
+}
+
+print(cnt)
