732459343562d57e2b3075055b0e2e9191ed0059
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-01-10 19:09:40 +0900

    [Gold IV] Title: 부분합, Time: 40 ms, Memory: 76292 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.swift
+++ 백준/Gold/1806. 부분합/부분합.swift
@@ -1,29 +1,33 @@
-// 0.5초
-// 128MB
-
-// 10000이하 N개 자연수 수열
-// 부분합이 S이상 가장 짧은 것
-
-// N: 10~10만개
-// S: 0~1억
-
 let NS = readLine()!.split(separator: " ").map { Int($0)! }
 let N = NS[0]
 let S = NS[1]
 var A = readLine()!.split(separator: " ").map { Int($0)! }
 
 var en = 0
-var tot = A[0]
-var ans = Int.max
+var tot = A[0] // st ~ en까지 합
+var cnt = Int.max
 
 for st in 0..<N {
-    while en < N && tot < S {
+    while tot < S {
         en += 1
-        if en != N { tot += A[en] }
+        if en < N { tot += A[en] }
+        else { break }
     }
-    if en == N { break }
-    ans = min(ans, en - st + 1)
-    tot -= A[st]
+    if en == N { break } // 이제 S보다 무조건 작다.
+    
+    // 이제 S에 도달했다.
+    cnt = min(cnt, en - st + 1) // 누적합에 쓰이는 원소 개수 최솟값 업데이트
+    tot -= A[st] // 이제 앞으로 전진
 }
 
-print(ans == Int.max ? 0 : ans)
+// O(2N)에 누적합 확인
+// 확인했는데 cnt가 업데이트 안됐다? -> 누적합 불가능
+print(cnt == Int.max ? 0 : cnt)
+
+// TestCase
+// 누적합으로 11을 만들 수 없는 경우
+// 10 11
+// 1 1 1 1 1 1 1 1 1 1
+// ----
+// 0
+
