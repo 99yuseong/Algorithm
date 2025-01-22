2b82051bafa641cc91db7eda86b572001633c570
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-01-22 14:10:56 +0900

    [Silver III] Title: N번째 큰 수, Time: 308 ms, Memory: 149928 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.swift
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.swift
@@ -58,15 +58,97 @@ final class FileIO {
     }
 }
 
+// 1초
+// 12MB -> Int 160만개 배열
 
+// 자신의 한칸 위의 수보다 크다.
+
+// N: 1~1500
+// 총 2250000개의 수
+// -10억 ~ 10억 사이의 정수
+
+// N개의 배열에 1개씩 값을 추가 -> 이미 정렬된 배열
+// N번째 큰 수까지 Max -> O(N^2)
 let fIO = FileIO()
 
 let N = fIO.readInt()
-var arr = [Int]()
+var heap = Heap()
 
+// O(N^2)
 for _ in 0..<N*N {
-    let input = fIO.readInt()
-    arr.append(input)
+    heap.insert(fIO.readInt())
 }
-arr.sort()
-print(arr[N*N-N])
+
+// O(NlgN)으로 출력
+for _ in 0..<N-1 { _ = heap.pop() }
+print(heap.pop()!)
+
+struct Heap {
+    var heap: [Int] = [0]
+    var compare: (Int, Int) -> Bool = { $0 > $1 }
+    
+    mutating func heapify() {
+        let startIdx = (heap.count-1) / 2
+        
+        for i in stride(from: startIdx, through: 1, by: -1) {
+            swimUp(i)
+        }
+    }
+    
+    mutating func insert(_ k: Int) {
+        if heap.isEmpty {
+            heap.append(k)
+            heap.append(k)
+            return
+        }
+        
+        heap.append(k)
+        swimUp(heap.count-1)
+    }
+    
+    mutating func swimUp(_ idx: Int) {
+        var curIdx = idx
+        
+        while curIdx > 1 && compare(heap[curIdx], heap[curIdx/2]) {
+            heap.swapAt(curIdx, curIdx/2)
+            curIdx /= 2
+        }
+    }
+    
+    mutating func pop() -> Int? {
+        if heap.count < 2 { return nil }
+        if heap.count == 2 { return heap.removeLast() }
+        
+        heap.swapAt(1, heap.count-1)
+        let result = heap.removeLast()
+        swimDown(1)
+        return result
+    }
+    
+    mutating func swimDown(_ idx: Int) {
+        var curIdx = idx
+        
+        while true {
+            let left = curIdx * 2
+            let right = curIdx * 2 + 1
+            var swapIdx = curIdx
+            
+            if left < heap.count && compare(heap[left], heap[swapIdx]) {
+                swapIdx = left
+            }
+            
+            if right < heap.count && compare(heap[right], heap[swapIdx]) {
+                swapIdx = right
+            }
+            
+            if swapIdx != curIdx {
+                heap.swapAt(curIdx, swapIdx)
+                curIdx = swapIdx
+            } else {
+                break
+            }
+        }
+    }
+}
+
+
