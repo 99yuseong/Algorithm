a0747d822eb580fce5c585e3aa29244f07e78544
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-02-05 12:27:40 +0900

    [level 1] Title: K번째수, Time: 0.14 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    
+    var result: [Int] = []
+    
+    for cmd in commands { // 50번
+        var arr = Array(array[cmd[0]-1...cmd[1]-1]).sorted() // (100 + 100) * 100lg100
+        result.append(arr[cmd[2]-1])    
+    }
+    
+    return result
+}
+
+// array: 1~100이하
+// 원소 1~100이하
+// commands: 1~50이하
+
+// cmd[0] ~ cmd[1]까지 배열 슬라이싱
+// sort
+// k번째 요소 result에 주입
