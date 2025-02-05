e5cb8c36d9ff3aeece814758b326e8de55c9f9ff
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-02-05 13:13:27 +0900

    [level 2] Title: H-Index, Time: 2.48 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func solution(_ citations:[Int]) -> Int {
+    
+    var C = citations.sorted(by: >)
+    var maxH = 0
+    
+    for i in 0..<C.count {
+        if i+1 <= C[i] { 
+            maxH = i+1
+        } else {
+            break
+        }
+    }
+    
+    return maxH
+}
+
+// 논문 n편, h번 이상 인용된 논문 h편 이상
+// 나머지 논문이 h번 이하 인용되었다면
+// h의 최댓값이 H-Idx
+
+// n: 1~1000
+// h: 0~10000
+
+// 길이 n의 citations -> O(n^3)
+// 작은순 정렬 -> O(nlgn)
+// 뒤에서부터, idx+1 == h인 h 찾기 -> O(n)
+// O(n) + O(nlgn)
