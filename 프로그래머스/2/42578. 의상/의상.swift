84d76c3a939317e2e4bce198b296e2efdb0923f0
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-02-07 01:16:36 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func solution(_ clothes:[[String]]) -> Int {
+    var D: [String: Int] = [:]
+    
+    for cloth in clothes {
+        if D[cloth[1]] == nil { D[cloth[1]] = 1 }
+        else { D[cloth[1]] = D[cloth[1]]! + 1 }
+    }
+    
+    return D.values.reduce(1) { $0 * ($1+1) } - 1
+}
+
+// 옷의 조합
+// 종류별 최대 1가지 의상
+// 의상 종류
+// [a,b] [c] [d] [e]
+// 같은 의상 x
+// 문자열의 길이는 1~20 자연수
+
+// Dictionary -> [String: Int] 갯수 카운트
+// Dictionary.keys.reduce(1) { $0 * ($1 + 1) } - 1
