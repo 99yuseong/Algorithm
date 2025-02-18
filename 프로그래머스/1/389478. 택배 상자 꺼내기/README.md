b864db9d0b3a7faef3eefbafa9c9c011c9b29773
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-02-19 00:26:33 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 택배 상자 꺼내기 - 389478 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478#) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 0.01 ms
+메모리: 16.5 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 19일 00:23:47
+2025년 02월 19일 00:26:31
 
 ### 문제 설명
 
--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.swift
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.swift
@@ -8,14 +8,12 @@ func solution(_ n:Int, _ w:Int, _ num:Int) -> Int {
     
     // n-1 / w -> 짝수: 오른쪽으로, 홀수: 왼쪽으로
     var top = (n-1) / w + 1
-    var isTopRight = top % 2 == 1
     var topBox = (n-1) % w + 1
     
     var target = (num-1) / w + 1
-    var isTargetRight = target % 2 == 1
     var targetBox = (num-1) % w + 1
     
-    if (isTargetRight && isTopRight) || (!isTargetRight && !isTopRight) { 
+    if top % 2 == target % 2 { 
         return top - target + (topBox >= targetBox ? 1 : 0)   
     } else {
         return top - target + (topBox >= w-targetBox ? 1 : 0)
