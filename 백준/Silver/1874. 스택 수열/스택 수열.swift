58fbfba99e239bde26e75b0b235f99f8c626f669
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-02-24 14:53:51 +0900

    [Silver II] Title: 스택 수열, Time: 60 ms, Memory: 71560 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.swift
+++ 백준/Silver/1874. 스택 수열/스택 수열.swift
@@ -1,26 +1,36 @@
-import Foundation
-
-let n = Int(readLine()!)!
-var stack: [Int] = []
-var result: [String] = []
-var curValue = 1
-
-for _ in 0..<n {
-    let input = Int(readLine()!)!
+func solution(n: Int, arr: [Int]) -> String {
+    // 1~n까지의 수 stack 넣기
+    // push 순서는 오름차순
+    // 수열을 만들 수 있는가?
+    // 모두 다른 정수
+    // N: 1~10만 - O(N) / O(NlgN)
     
-    while curValue <= input {
-        stack.append(curValue)
-        result.append("+")
-        curValue += 1
+    var stack: [Int] = []
+    var ans = ""
+    var curIdx = 0
+    
+    for i in 1...n {
+        while !stack.isEmpty && curIdx < n && arr[curIdx] == stack.last! {
+            stack.removeLast()
+            ans += "-\n"
+            curIdx += 1
+        }
+        
+        stack.append(i)
+        ans += "+\n"
     }
     
-    if stack.last == input {
-        _ = stack.popLast()
-        result.append("-")
-    } else {
-        print("NO")
-        exit(0)
+    while !stack.isEmpty && curIdx < n && arr[curIdx] == stack.last! {
+        stack.removeLast()
+        ans += "-\n"
+        curIdx += 1
     }
+    
+    return stack.isEmpty ? ans : "NO"
 }
 
-print(result.joined(separator: "\n"))
+let N = Int(readLine()!)!
+var arr: [Int] = []
+for _ in 0..<N { arr.append(Int(readLine()!)!) }
+
+print(solution(n: N, arr: arr))
