03c4c3005bbe166e31de6b10540df5a7c6632249
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-02-24 15:17:06 +0900

    [Silver IV] Title: 카드2, Time: 24 ms, Memory: 77176 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.swift
+++ 백준/Silver/2164. 카드2/카드2.swift
@@ -1,13 +1,52 @@
-import Foundation
-
-let n = Int(readLine()!)!
-var cards = Array(1...n)
-var index: Int = 0
-
-while cards.count - 1 != index {
-    index += 1
-    cards.append(cards[index])
-    index += 1
+func solution(N: Int) -> Int {
+    
+    // N장의 카드
+    // 1~N까지의 번호
+    // 1번이 위 ~ N번이 아래
+    
+    // 1. 맨위를 버린다.
+    // 2. 맨위를 맨 아래로.
+    
+    // 1234
+    // 1. 1버림
+    // 2. 342
+    // 3. 3버림
+    // 4. 24
+    // 5. 2버림
+    // 6. 4
+    // 마지막 남는 카드
+    
+    // N: 1~50만 -> O(N)
+    
+    struct Deque {
+        var inbox: [Int] = []
+        var outbox: [Int] = []
+        var count: Int { inbox.count + outbox.count }
+        var top: Int? { outbox.isEmpty ? inbox.first : outbox.last }
+        mutating func pop() -> Int? {
+            if outbox.isEmpty {
+                outbox = inbox.reversed()
+                inbox.removeAll()
+            }
+            return outbox.popLast()
+        }
+        mutating func push(_ x: Int) {
+            inbox.append(x)
+        }
+    }
+    
+    var DQ = Deque()
+    for i in 1...N {
+        DQ.push(i)
+    }
+    
+    while DQ.count > 1 {
+        DQ.pop()
+        DQ.push(DQ.pop()!)
+    }
+    
+    return DQ.top!
 }
 
-print(cards.last!)
+let N = Int(readLine()!)!
+print(solution(N: N))
