64ae0d1d22b326fe32cee592846d9e752eead99d
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-02-24 15:05:43 +0900

    [Silver IV] Title: 스택, Time: 20 ms, Memory: 69544 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.swift
+++ 백준/Silver/10828. 스택/스택.swift
@@ -1,21 +1,34 @@
-import Foundation
+func solution(N: Int, cmds: [String]) -> [Int] {
+    var stack: [Int] = []
+    var ans: [Int] = []
+    
+    for cmd in cmds {
+        let _cmd = cmd.split(separator: " ")
+        
+        switch _cmd[0] {
+            case "push":
+                stack.append(Int(_cmd[1])!)
+            case "pop":
+                ans.append(stack.isEmpty ? -1 : stack.removeLast())
+            case "size":
+                ans.append(stack.count)
+            case "empty":
+                ans.append(stack.isEmpty ? 1 : 0)
+            case "top":
+                ans.append(stack.isEmpty ? -1 : stack.last!)
+            default: break
+        }
+    }
+    
+    return ans
+}
 
 let N = Int(readLine()!)!
-var stack: [Int] = []
+var cmds: [String] = []
+for _ in 0..<N { cmds.append(readLine()!) }
 
-for _ in 0..<N {
-    let cmd = readLine()!.split(separator: " ")
-    
-    switch cmd.first! {
-    case "push":
-        stack.append(Int(cmd.last!)!)
-    case "pop":
-        print(stack.popLast() ?? -1)
-    case "size":
-        print(stack.count)
-    case "empty":
-        print(stack.isEmpty ? 1 : 0)
-    default: // top
-        print(stack.last ?? -1)
-    }
+var ans = ""
+for num in solution(N: N, cmds: cmds) {
+    ans += "\(num)\n"
 }
+print(ans)
