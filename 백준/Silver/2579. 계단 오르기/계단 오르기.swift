d68f4fd898535fe9eb7d5586b6ab73dcaddfeed6
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-02-27 14:03:25 +0900

    [Silver III] Title: 계단 오르기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.swift
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.swift
@@ -1,19 +1,37 @@
-import Foundation
+// 한 계단 / 두 계단 씩
+// 연속된 3개의 계단 밟기 안됨
+// 마지막 도착은 밟아야 한다.
+
+// N: 계단 개수, 1~300, 점수: 1~1만
+func solution(_ N: Int, _ S: [Int]) -> Int {
+    // D[i]: i번째 계단을 밟았을 때까지의 최대 점수
+    // 2, 1로 온 경우
+    // 2로 온 경우
+    // D[i] = D[i-3] + S[i-1] + S[i]
+    // D[i] = D[i-2] + S[i]
+    
+    // D[1] = S[1]
+    // D[2] = S[1] + S[2]
+    
+    let S = [0] + S
+    if N == 1 { return S[1] }
+    if N == 2 { return S[1] + S[2] }
+    
+    var D = Array(repeating: 0, count: N+1)
+    D[1] = S[1]
+    D[2] = S[1] + S[2]
+    
+    for i in 3...N {
+        D[i] = max(D[i-3] + S[i-1] + S[i],
+                   D[i-2] + S[i])
+    }
+    
+    return D[N]
+}
 
 let N = Int(readLine()!)!
-var score = Array(repeating: 0, count: N+1)
-for i in 1...N { score[i] = Int(readLine()!)! }
-
-if N == 1 { print(score[1]); exit(0) }
-if N == 2 { print(score[1]+score[2]); exit(0) }
-
-var D = Array(repeating: Array(repeating: 0, count: 3), count: N+1)
-D[1][1] = score[1]
-D[2][1] = score[2]
-D[2][2] = score[1] + score[2]
-
-for i in 3...N {
-    D[i][1] = max(D[i-2][1], D[i-2][2]) + score[i]
-    D[i][2] = D[i-1][1] + score[i]
+var S: [Int] = []
+for _ in 0..<N {
+    S.append(Int(readLine()!)!)
 }
-print(max(D[N][1], D[N][2]))
+print(solution(N, S))
