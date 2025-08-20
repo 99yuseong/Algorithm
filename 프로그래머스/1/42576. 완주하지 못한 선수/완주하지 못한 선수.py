ad7f78ff1f3b5e30b36fc31de0777caf29273433
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-08-20 12:16:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.83 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,32 +1,28 @@
 def solution(participant, completion):
     
-    dict = {}
+    count_dict = {}
     
-    for man in participant:
-        if man in dict:
-            dict[man] += 1
-        else:
-            dict[man] = 1
-            
-    for man in completion:
-        if man in dict:
-            dict[man] -= 1
-        else:
-            break
+    for p in participant:
+        count_dict[p] = count_dict.get(p, 0) + 1
     
-    answer = ''
-    for k, v in dict.items():
+    for p in completion:
+        count_dict[p] = count_dict.get(p, 0) - 1
+    
+    for k, v in count_dict.items():
         if v == 1:
-            answer = k
-            break
-            
-    return answer
-
-
-# 한명만 완주 못함
-# 참여한 선수 이름 / 완주한 선수 이름
-# 완주 못한사람을 리턴해라
+            return k
 
 # 10만 - O(N^2)
 # 알파벳 소문자로 구성
 # 동명이인 가능
+        
+# 동명이인 처리
+        
+# 동명이인 -> 단순 set으로 처리 불가
+# Dictionay 또는 Counter로 처리
+# 효율적인 자료구조, 동명이인 처리, 차집합 로직
+# Counter 딕셔너리 서브 클래스 -> 등장횟수, 연산 가능
+
+# 한명만 완주 못함
+# 참여한 선수 이름 / 완주한 선수 이름
+# 완주 못한사람을 리턴해라
