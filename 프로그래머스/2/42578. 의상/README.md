3824247b3a70c76f62dc53bfa4ffe968e755c7a7
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-08-21 10:56:52 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.2 MB, 시간: 0.08 ms
+메모리: 16.5 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 07일 01:21:06
+2025년 08월 21일 10:56:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.swift
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -1,12 +1,47 @@
 import Foundation
 
 func solution(_ clothes:[[String]]) -> Int {
-    var D: [String: Int] = [:]
+    var dict: [String: Int] = [:]
+
+    for cloth in clothes {
+        dict[cloth[1], default: 0] += 1
+    }
     
-    clothes.forEach { D[$0[1], default: 0] += 1 }
+    var result = 1
     
-    return D.values.reduce(1) { $0 * ($1+1) } - 1
+    for value in dict.values {
+        result *= value + 1
+    }
+    
+    return result - 1
 }
+
+
+// 각 종류별 최대 1가지
+// 최소 1가지 이상 의상
+// 서로 다른 옷의 조합 수 리턴
+
+// 이름, 종류
+// N: 1~30
+// 모든 원소는 문자열 - 소문자 or _
+
+// (2 + 1) * (1 + 1) - 1
+// (3 + 1) - 1
+
+// 1. dictionary에 해당 원소를 종류를 key로 + 1로 카운트
+// 2. values()에 1을 더한후 모두 곱한다.
+// 3. 최종적으로 1을 뺀다. (모두 0인 케이스)
+
+
+
+
+
+
+
+
+
+
+
 
 // 옷의 조합
 // 종류별 최대 1가지 의상
