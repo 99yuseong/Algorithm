3dd34948f51526ada6c63ccd04ed63831b97bbe9
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-08-24 14:26:10 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 109.07 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
@@ -1,51 +1,77 @@
+// 트럭 여러대
+// 일차선 도로 - 정해진 순
+// 다 건너려면 몇 초가 걸리나?
+
+// 다리에는 최대 bridge_length대 만큼
+// 그리고 weight를 견딜 수 있다.
+
+// 완전히 오르지 않는 트럭은 무시
+
+// 2대 / 10kg
+// 7 4 5 6
+// __ 0
+// _7 1
+// 7_ 2
+// _4 3
+// 45 4
+// 5_ 5
+// _6 6
+// 6_ 7
+// __ 8
+
+// 다리를 다 건너려면 최소 몇 초가 걸리냐?
+
+// N: 1~1만
+// M: 1~1만
+
+// 선입 선출 -> Queue
+// 1대가 건너려면 bridge_length초가 걸리지
+// bridge_length * truck_weights => 1만 * 1만 ok
+
+// 1. 현재 차량 수 ok?
+// 2. 현재 무게 ok?
+// 3. ok! 그러면 enqueue
+// 4. no! 그러면 대기
+// 5. 다리에 차가 존재 -> queue.count로 확인
+//    & 위치 -> 현재 시각 - 넣은 시각
+//    queue: [(weight, time)]
+// 6. truck_weights가 비어있고, 다리가 비어있을때 종료
+
+// t (현재시각)
+// for truck in truck_weights
+//   loop queue not empty 
+
 import Foundation
-
-// 트럭 - 일차선 다리 건너기
-// 모든 트럭이 건너려면 몇초?
-
-// 최대 birdge_length대
-// weigth만큼 견뎌
-// 완전히 오르지 않는 트럭 무시
-
-// 1초 다리 - 1초 지남
-
-// bl: 1~1만
-// w: 1~1만
-// tw: 1~1만
-
-// 최소 몇초
-// 다리 건너는데 - bl초
 
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
     
-    // 1. t가 흐른다.
-    // 2. 만약 Queue[1][1] == t라면 해당 트럭 탈출, Done += 1, w -= 탈출한 트럭 무게
-    // 3. curW + nextW <= weight라면
-    //  Queue에 [nextW, t+bridge_len] 추가, w += nextW, idx += 1
-    // 1,2,3을 D == truck_weights.count 까지 반복
+    var curT = 0
+    var curW = 0
+    var queue: [(w: Int, t: Int)] = []
+    var truckWeights = Array(truck_weights.reversed())
     
-    
-    var t = 0
-    var idx = 0
-    var w = 0
-    var Done = 0
-    var Queue: [[Int]] = []
-    
-    while Done < truck_weights.count {
-        t += 1
+    while !queue.isEmpty || !truckWeights.isEmpty {
         
-        if !Queue.isEmpty && Queue[0][1] == t {
-            let done = Queue.removeFirst()
-            Done += 1
-            w -= done[0]
+        curT += 1
+        
+        // 차량 끝 도착
+        if !queue.isEmpty 
+        && (curT - queue.first!.t) >= bridge_length 
+        {
+            curW -= queue.first!.w
+            queue.removeFirst() 
         }
         
-        if idx < truck_weights.count && w + truck_weights[idx] <= weight {
-            Queue.append([truck_weights[idx], t + bridge_length])
-            w += truck_weights[idx]
-            idx += 1
+        // 차량 다리 출발
+        if !truckWeights.isEmpty 
+        && bridge_length >= queue.count + 1
+        && weight >= curW + truckWeights.last!
+        {
+            let truckW = truckWeights.popLast()!
+            queue.append((w: truckW, t: curT))
+            curW += truckW
         }
     }
     
-    return t
+    return curT
 }
