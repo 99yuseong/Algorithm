132af3a6813765967aab98971a481676dbf15a2a
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-08-25 09:50:57 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.2 MB, 시간: 0.05 ms
+메모리: 16.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 04일 18:10:58
+2025년 08월 25일 09:50:54
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -1,40 +1,59 @@
 import Foundation
 
-// 진도 100에 서비스 반영
-// 뒷 기능은, 앞 기능 배포시 함께 배포
-// 배포 순서대로 p배열
-// 각각의 속도 s배열
-// 배포마다 몇개의 기능이 배포 되는 지를 return
+// 기능 개선 작업
+// 진도가 100%일때 서비스에 반영 가능
+// 개발 속도는 모두 다르다.
+// 뒤에 있는 기능은 앞 기능이 배포될때 함께배포 !
 
-// 작업은 100개 이하
-// 진도는 100미만 (0~99짜리 진도)
-// 속도는 100이하
-// 배포는 하루 1번(끝)
+// 진도 배열, 속도 배열
 
-// 93 30 55
-// 1 30 5
+// N: 1~100
+// 1~100사이의 수 -> Int로 처리해도 된다.
+// 배포는 1번 하루의 끝
 
-// 1일 - 94 60 60
-// 2일 - 95 90 65
-// 3일 - 96 100 70
-// ...
+// 93, 30, 55
+// 각 배포때마다 몇개의 작업이 배포되는가?
+
+// 시간이 중요한건 아니다.
+// 1. progress를 돌면서 필요한 일 수 계산.
+// 2. 총 지나간 일 수 업데이트
+// 2. 해당 일수가 지났을때, 진도가 100? 통과
+
+// loop: 배포된 건 수로 카운트 -> progresses의 갯수와 동일할때까지
+    // 맨 앞건 필요 일수 계산. d = (100 - p + 1) / speed
+    // loop: 진도율 확인하며 p + (d * speed) >= 100 만족 시 배포에 포함
+
+// 최종 값 리턴
 
 func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
     
-    var lastReleaseDate: Int = 0
-    var numOfReleases: [Int] = []
+    var deploy: [Int] = []
+    var deployCount = 0
+    var passedDay = 0
     
-    for i in 0..<progresses.count {
-        let progress = Double(progresses[i])
-        let speed = Double(speeds[i])
+    while deployCount < progresses.count {
+        
+        let progress = Double(progresses[deployCount])
+        let speed = Double(speeds[deployCount])
         let day = Int(ceil((100 - progress) / speed))
-    
-        if day > lastReleaseDate {
-            lastReleaseDate = day
-            numOfReleases.append(1)
-        } else {
-            numOfReleases[numOfReleases.count - 1] += 1
+        
+        var curDeployCount = 1
+        deployCount += 1
+        passedDay = day
+        
+        while deployCount < progresses.count 
+        && progresses[deployCount] + passedDay * speeds[deployCount] >= 100 {
+            curDeployCount += 1
+            deployCount += 1
         }
+        
+        deploy.append(curDeployCount)
     }
-    return numOfReleases
+    
+    return deploy
 }
+
+
+
+
+
