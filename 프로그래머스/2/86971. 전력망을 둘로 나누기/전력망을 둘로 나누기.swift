6c5bf0fecb369b72f9b6d5d30de3b7c70a84db74
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-08-29 10:47:24 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 18.78 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.swift
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.swift
@@ -1,72 +1,61 @@
-import Foundation
+// n개의 송전탑
+// 1개를 끊어 2개로 분할 -> 최대한 비슷하게
 
-struct Queue<T> {
-    var inbox = [T]()
-    var outbox = [T]()
-    
-    var isEmpty: Bool { inbox.isEmpty && outbox.isEmpty }
-    mutating func append(_ k: T) { inbox.append(k) }
-    mutating func removeFirst() -> T? {
-        if outbox.isEmpty { 
-            outbox = inbox.reversed()
-            inbox.removeAll()
-        }
-        return outbox.popLast()
-    }
-}
-
-func solution(_ n:Int, _ wires:[[Int]]) -> Int {
-    
-    var ans = n
-    
-    for i in 0..<wires.count {
-        var Connected: [[Int]] = Array(repeating: [], count: n+1)
-        
-        for wire in wires {
-            guard wire != wires[i] else { continue }
-            
-            let a = wire[0]
-            let b = wire[1]
-            
-            Connected[a].append(b)
-            Connected[b].append(a)
-        }
-        
-        var k = 0
-        var queue = Queue<Int>()
-        queue.append((i+1) % n + 1)
-        var visited: [Bool] = Array(repeating: false, count: n+1)
-        
-        while !queue.isEmpty {
-            let cur = queue.removeFirst()!
-            
-            for top in Connected[cur] {
-                if !visited[top] {
-                    visited[top] = true
-                    queue.append(top)
-                    k += 1
-                }
-            }
-        }
-        
-        if abs(n - k * 2) < ans { ans = abs(n - k * 2) }
-    }
-    
-    return ans
-}
+// return: 송전탑 개수 절댓값 차이 최솟값
 
 // n: 2~100
-// wires: n-1길이의 정수형 이차원 배열
+// [v1, v2]
 
-// 하나의 트리 형태
+// [ [3], [3], [1,2], [3,5,6,7] ] -> O(N)
+// 1번의 연결된 갯수 찾기
 
-// 송전탑의 개수 n,
-// 전선정보 wires
-// 전선 하나를 끊어서 송전탑 개수가 비슷하도록 나누었을때, 송전탑의 개수 차이를 리턴
+// wires를 돌면서 특정 wire 제외 
+// -> vertex 형태로 변환
+// -> 0번 인덱스와 연결된 cnt를 dfs로 구현
+// -> abs((wires.count - cnt) - cnt)의 min값 구하기
 
-// 1. wires loop 
-//    -> i번째를 제외한 탑 배열 선언
-//    -> n번째 탑과 연결된 탑 배열 - 이차원 배열 선언
+import Foundation
 
-// 2. BFS 돌면서 연결된 탑의 개수 k 계산 -> O(N)
-// 3. abs(n - k - k) 최솟값 계산
+func solution(_ n:Int, _ wires:[[Int]]) -> Int {
+    var minDiff = n
+    
+    for i in 0..<wires.count {
+        // i번째 wire를 끊기
+        var newWires = wires
+        newWires.remove(at: i)
+        
+        let vertexs = makeVertexs(n, newWires)
+        let cnt = bfs(vertexs)
+        minDiff = min(abs((n - cnt) - cnt), minDiff)
+    }
+    
+    return minDiff
+}
+
+func makeVertexs(_ n: Int, _ wires: [[Int]]) -> [[Int]] {
+    var vertexs: [[Int]] = Array(repeating: [], count: n+1)
+    for wire in wires {
+        vertexs[wire[0]].append(wire[1])
+        vertexs[wire[1]].append(wire[0])
+    }
+    return vertexs
+}
+
+func bfs(_ vertexs: [[Int]]) -> Int {
+    var queue: [Int] = [1]   
+    var visited = Array(repeating: false, count: vertexs.count)
+    visited[1] = true
+    var cnt = 1
+    
+    while !queue.isEmpty {
+        let cur = queue.removeFirst()
+        for near in vertexs[cur] {
+            if !visited[near] {
+                queue.append(near)
+                visited[near] = true
+                cnt += 1
+            }
+        }
+    }
+    return cnt
+}
