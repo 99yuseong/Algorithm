59a9606aeaa65314e3f5e82b66924fdff552dcb2
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-08-29 10:13:05 +0900

    [level 2] Title: 피로도, Time: 25.84 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.swift
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -1,43 +1,52 @@
+// 피로도 0 이상 정수 == 체력
+// 최소 피로도, 소모 피로도
+
+// 현재 피로도 k, [최소, 소모]
+// 탐험할 수 있는 최대 던전 수
+
+// k: 1~5000
+// d: 8개 이하, [최소, 소모] -> 같을 수도 있다.
+// 최소 >= 소모
+// 최소, 소모: 1~1000
+
+// 그리디 -> 최소 피로도 높을 수록, 소모 피로도 낮을 수록 정렬
+// 근데, 최소 피로도가 높다는 건 소모피로도도 같이 높을 수 있다. -> 그리디 안됨
+
+// 8개면 그냥 줄세워보고 확인해봐라.
+// 8! ~~ 500정도
+
+// maxCnt = 0
+// var k를 기준으로, 8개 다 돌았거나, 0이 된 경우 체크
+// visited
+// dfs로 8개 다 돌았을 때 체크
+
 import Foundation
 
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
     
-    // 던전 줄 세우기, && 탐험
-    var piro = k
-    var explore = 0
-    var isUsed = Array(repeating: false, count: k)
+    var maxCnt = 0
+    var visited = Array(repeating: false, count: dungeons.count)
     
-    func permu(_ n: Int, _ curPiro: Int, _ curExplore: Int) {
+    func dfs(_ k: Int, _ n: Int, _ cnt: Int) {
+        
         if n == dungeons.count {
-            explore = curExplore
+            maxCnt = max(maxCnt, cnt)
             return
         }
         
         for i in 0..<dungeons.count {
-            if !isUsed[i] {
-                isUsed[i] = true
-                if curPiro >= dungeons[i][0] {
-                    permu(n+1, curPiro - dungeons[i][1], curExplore+1)
+            if !visited[i] {
+                visited[i] = true
+                if k >= dungeons[i][0] {
+                    dfs(k-dungeons[i][1], n+1, cnt+1)
                 } else {
-                    explore = max(explore, curExplore)
+                    dfs(k, n+1, cnt)
                 }
-                isUsed[i] = false
-            } 
+                visited[i] = false
+            }
         }
-    }  
-    permu(0, k, 0)
+    }
+    dfs(k, 0, 0)
     
-    return explore
+    return maxCnt
 }
-
-// 한번씩 탐험 던전
-// 현재 피로도 k
-// 각 던전별 최소 피로도, 소모피로도
-// 탐험할 수 있는 최대 던전 수
-
-// k: 1~5000
-// 던전은 1~8개
-// 최소 피로도 >= 소모 피로도
-
-// 8개 다 돌아봐라? 이건가?
-// 8! -> 대강 6천 언저리 / 그리디 필요 X, 
