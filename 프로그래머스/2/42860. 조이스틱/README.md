6205bc0eac7f8ded6a483132767a5f4ed8c69069
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-09-02 11:20:15 +0900

    [level 2] Title: 조이스틱, Time: 2.24 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=swift#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.2 MB, 시간: 0.04 ms
+메모리: 16.1 MB, 시간: 2.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 15:11:50
+2025년 09월 02일 11:20:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.swift
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.swift
@@ -1,52 +1,45 @@
+// A
+
+// 좌우 - 커서 이동
+// 위아래 - 알파벳 변경
+
+// 최소 조작횟수
+
+// 대문자로만, Name은 1~20
+
+// 좌 -> 우
+// 위 아래로 변경하면 되는거 아닌가?
+
+// 아스키코드 값 차이
+
 import Foundation
 
 func solution(_ name:String) -> Int {
     
     let n = name.count
+    let arr = Array(name)
+    
+    // 위 아래 이동
     var upDown = 0
-    for char in name {
-        upDown += changeAlphabetCnt(of: char) 
+    for char in arr {
+        let diff = abs(Int(char.asciiValue!) - Int(Character("A").asciiValue!))
+        upDown += min(diff, abs(26 - diff))
     }
-
-    var leftRight = n-1
-    var arr = Array(name)
+    
+    // 좌우 이동
+    var leftRight = n - 1
     for i in 0..<n {
-        // 현재점부터 "A"가 아닌 문자가 오는 지점 next를 찾기
-        // 0~i랑 n-next랑 더한다음, 그중 더 작은것을 더해야함
-        var next = i+1
+        
+        var next = i + 1
         while next < n && arr[next] == "A" {
             next += 1
         }
         
-        // 최종적으로 계속 최솟값을 업데이트
-        leftRight = min(leftRight, (i - 0) + (n - next) + min(i, n - next))
+        let left = i - 0
+        let right = n - next
+        
+        leftRight = min(leftRight, left + right + min(left, right))
     }
     
     return upDown + leftRight
-}
-
-// 조이스틱으로 알파벳이름
-// A
-
-// AAAA -> NAME 이런식으로 바꿔라
-
-// 위: 다음
-// 아래: 이전 알파벳
-// 왼: 커서 왼쪽으로
-// 오: 커서 오른쪽으로
-
-// JAZ
-// 9번조작
-// 위 아래 중 가까운 수만큼 아스키밸류 -> 차만큼 이동
-
-// A가 나오냐 안나오냐의 싸움?
-// name의 길이는 1~20
-
-// 1. 우선 A와 가깝나, Z와 가깝나.
-func changeAlphabetCnt(of char: Character) -> Int {
-    if char <= "N" {
-        return Int(char.asciiValue! - Character("A").asciiValue!)
-    } else {
-        return Int(Character("Z").asciiValue! - char.asciiValue! + 1)
-    }
 }
