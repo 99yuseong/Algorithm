5d3c7129599259512b8f4915180ad89614012087
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2025-09-03 10:32:26 +0900

    [level 2] Title: 큰 수 만들기, Time: 349.39 ms, Memory: 21.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.swift
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.swift
@@ -1,42 +1,42 @@
+// n: 2~100만
+// k: 1~100만
+// k < n (무조건 1개는 남는다.)
+// Int로 변환하면 무조건 터짐
+
+// O(N) or O(NlgN) 안쪽으로
+
+// 큰 수 = 맨 앞 자리가 크면 클수록
+
+// 앞에 k개 중 max1
+// k - max1의 idx를 하고 반복
+// 최악의 경우, O(k^2)
+
+// stack
+// []
+// []
+
+// [1] 9 - 2 - 4
+// 첫 값을 넣고, 
+// number를 돌면서 자기보다 큰 가
+// 자기보다 큰 값을 
+
 import Foundation
 
 func solution(_ number:String, _ k:Int) -> String {
     
+    let n = number.count
     var stack: [Int] = []
-    var k = k
+    var removeCnt = 0
     
-    for num in number.map { Int(String($0))! } {
-        while !stack.isEmpty && stack.last! < num && k > 0 { 
+    for num in number {
+        let num = Int(String(num))!
+        
+        while !stack.isEmpty && num > stack.last! && k > removeCnt {
             stack.removeLast()
-            k -= 1
+            removeCnt += 1
         }
-        stack.append(num)
+        stack.append(num) 
     }
     
-    if k > 0 { stack.removeLast(k) }
-    
-    return stack.map { String($0) }.joined()
+    return stack.prefix(n-k).map { String($0) }.joined()
 }
-
-// 어떤 숫자에서 k개의 수를 제거했을때, 얻을 수 있는 가장 큰 숫자
-
-// number: 2자리~100만자리
-// 100만C50만 이거 안됨
-
-// 1. 일단 다 제거?
-//  안됨. 시간 초과
-
-// 2. 그리디?
-// - 가장 작은 수를 빼면 되나? NO, 자릿수가 가장 중요할 듯
-// 어짜피 숫자를 뺴도 전체 자릿수는 동일, 즉, 앞에서부터 k개 중 가장 큰 수만큼 남겨야함
-
-// 앞의수 k개 중 max 찾고, 그 앞에있는거 m개 빼고
-// 그 다음 m개 중 max 찾고, 그 앞에 있는거 n개 빼고
-// ...
-// 최악, 100만 * 100만 -> O(N^2)
-// O(N) 이나 O(NlgN)
-
-// Stack? 
-// [] 1. 자기보다 큰 값 만나면 k개 다찰때까지 제거
-//    2. 작은 값 만나면 넣기
-// 
