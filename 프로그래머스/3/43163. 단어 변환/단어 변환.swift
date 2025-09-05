3542b826e1a7975bce37df1775fdbbc38c37fdd4
Nam Yuseong <dbtjd6511@gmail.com>
2025-09-05 10:15:29 +0900

    [level 3] Title: 단어 변환, Time: 0.56 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -0,0 +1,77 @@
+// begin -> target 변환 
+// 몇단계 과정을 거쳐 변환할 수 있는 지를 리턴
+
+// 알파벳 소문자
+// 단어: 3~10 길이
+// words: 3~50개의 단어, 중복 X
+// begin != target
+// 변환 불가 시 0을 리턴
+
+// 1. target이 words안에 존재? -> 없으면 0을 리턴
+// 2. words를 돌면서 visited가 아닌 상태에서, 1개가 차이나는 지 확인
+// 3. 1개 차이나면 진입. count+1
+// 4. target과 같으면 return
+
+// -> 단어 2개를 비교해서 다른거 개수가 1개인지를 리턴하는 함수
+// -> DFS를 돌아야하네
+
+import Foundation
+
+func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
+    
+    // 1. 변환할 수 없는 경우
+    guard words.contains(target) else { return 0 }
+    
+    let N = words.count
+    var visited = Array(repeating: false, count: N)
+    var result = N
+    
+    func dfs(_ k: Int, _ word: String) {
+        
+        if word == target { 
+            result = min(result, k)
+            return
+        }
+        
+        for i in 0..<N {
+            if !visited[i] && word.canChange(to: words[i]) {
+                visited[i] = true
+                dfs(k+1, words[i])
+                visited[i] = false
+            }
+        }
+    }
+    
+    dfs(0, begin)
+    
+    return result
+}
+
+extension String {
+    // 다른 것 개수가 1개인지를 리턴하는 함수 O(N)
+    func canChange(to str: String) -> Bool {
+        var diff = 0
+        let arrA = Array(self)
+        let arrB = Array(str)
+        
+        for i in 0..<self.count {
+            diff += arrA[i] != arrB[i] ? 1 : 0
+        }
+        
+        return diff == 1
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
