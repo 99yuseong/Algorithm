309c663d64782c70473a659cddd28e3430b05d40
Nam Yuseong <dbtjd6511@gmail.com>
2025-09-09 10:36:49 +0900

    [level 3] Title: 입국심사, Time: 752.47 ms, Memory: 21.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.swift
+++ 프로그래머스/3/43238. 입국심사/입국심사.swift
@@ -1,36 +1,55 @@
+// n명의 입국심사
+
+// 대기 n명, 각 심사관별 t
+// 모든 사람이 심사하는 최소 시간
+
+// 무조건 들어가는게 아니라 더 빠른걸 기다릴 수도 있음
+// n: 1~10억
+// t: 1~10억분
+// t_cnt: 1~10만
+
+// O(NlgN)안쪽으로 -> 이분탐색
+
+// 1. 정렬하고
+
+// 남은시간 + times의 최솟값으로 돌아야하는데
+
+// 최소 시간 안에 다 검사를 받을 수 있나?
+// t시간
+    // 가장 작은 t의 가능 사람 ~ 가장 높은 t의 가능 사람 합산 > n인가?
+
+// 1. times 정렬
+// 2. st = 0, end = 100만 * 1000만
+
 import Foundation
 
 func solution(_ n:Int, _ times:[Int]) -> Int64 {
-    var st = times.min()!
-    var en = times.max()! * n
+    
+    // 1. 정렬 - O(NlgN)
+    let times = times.sorted()
+    
+    // 2. 이분 탐색 - O(lgN)
+    var st = 0
+    var en = Int.max
     
     while st < en {
-        let mid = (st + en) / 2
-        let passed = times.reduce(0) { $0 + mid / $1 }
         
-        if passed >= n {  
-            en = mid
-        } else { // passed < n -> 시간 늘려야함
-            st = mid + 1
+        let t = (st + en) / 2
+        
+        var passed = 0
+        
+        for time in times {
+            if passed >= n { break }
+            passed += t / time
+        }
+        
+        // 충분한 시간
+        if passed >= n {
+            en = t
+        } else {
+            st = t + 1
         }
     }
     
     return Int64(st)
 }
-
-// 기다리는 사람수 n, 
-// 각 심시관이 심사를 하는데 걸리는 시간 times
-// 모든 사람이 심사를 받는데 걸리는 최솟값
-
-// n: 1~10억명
-// time: 1~10억분
-// times -> 1~10만명
-
-// 시간의 최솟값
-// 1초일때 몇명?
-// 2초일때 몇명?
-
-// parametric Search
-// 1. 1~10억에서 lowerbound 이분탐색 -> O(lg10억)
-// 2. 조건이 times / t를 합한게 n이 넘나? -> O(10만)
-// -> O(10만 * lg10억)
