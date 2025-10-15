84720b78b786d24c8f3f5cd122965f7981a74546
Nam Yuseong <dbtjd6511@gmail.com>
2025-10-15 14:55:54 +0900

    [level 2] Title: 완전범죄, Time: 0.58 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.swift
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.swift
@@ -1,65 +1,87 @@
 import Foundation
 
-// 흔적을 최소화
+// 문제
 
-// info: 흔적 정보
-// n: A 최소 검거 흔적 개수
-// m: B 최소 검거 흔적 개수
+// 도둑 A,B 물건 훔치면 흔적 남음 -> 흔적 최소화해야함
 
-// return: 둘다 안잡힐때, A의 흔적 개수의 최솟값 or -1
+// A 도둑: n개의 흔적 = 붙잡힘
+// B 도둑: m개의 흔적 = 붙잡힘
 
-// 흔적 1~3
+// 모두 붙잡히지 않으면서 모든 물건 훔칠때,
+// return A 도둑 흔적의 최솟값
+
+// 모두 붙잡힐 수 밖에 없다면
+// return -1
+
+
+// 조건
+
 // info: 1~40
+// 물건당 흔적: 1~3
 // n: 1~120
 // m: 1~120
 
-// A와 B가 팀이 되어 훔친다.
 
-// 1. 모든 경우의 수 - 시간 초과
-// i: 0~39까지 A or B -> 2^40
-// 1099,511,627,776 -> 시간 초과
+// 풀이
 
+// 완전탐색 -> A 또는 B가 40개의 물건 훔침 -> 2^40 경우의 수 (시간 초과)
+// 그리디 -> 현재 선택이 이전/다음에 영향을 줄 수 있음 (최적해 놓칠 수 있음)
+// DP -> a흔적과 b흔적 모든 케이스를 저장하면서 확장
 
-// 2. 그리디 or dp
+// D[i][b] = a : i번째 물건을 훔치고 B의 흔적이 b일때, A의 흔적 최솟값 a
 
-// DP - O(40)
+// 1. A가 물건을 훔쳤을때,
+// D[i][j] = min(D[i][j], D[i-1][j] + a)
 
-// D[i][0]: i번째 물건을 훔쳤을때, A의 흔적 최솟값
-// D[i][1]: i번째 물건을 훔쳤을때, B의 흔적 
+// 2. B가 물건을 훔쳤을때,
+// D[i][j+b] = min(D[i][j+b], D[i-1][j]) 단, j+b < m
 
-// D[i][0] = D[i-1][1] + info[i][1] >= m ? D[i-1][0] : D[i-1][0] + info[i][0]
-// D[i][1] = D[i][0] == D[i-1][0] ? D[i-1][1] + info[i][1] : D[i-1][1]
-
-// 어떤걸 먼저해야 좋을까 (그리디)
-// [A, B] 
-// - 우선 B가 작을수록 좋지. B한테 넘길 수 있으니까.
-// - A가 클수록 좋지. 더 적어지니까.
+// D[0][0] = a 
+// D[0][b] = 0 단, b < m
 
 func solution(_ info:[[Int]], _ n:Int, _ m:Int) -> Int {
-    let info = info.sorted { 
-        $0[0] - $0[1] == $1[0] - $1[1] 
-        ? $0[0] / $0[1] > $1[0] / $1[1] : $0[0] - $0[1] > $1[0] - $1[1] 
-    }
-    var D = [[Int]](repeating: [-1, -1], count: info.count)
     
-    if info[0][1] >= m 
-    && info[0][0] >= n {
-        return -1
+    let MAXB = 120
+    let INF = 1_000_000_000
+    
+    var D = [[Int]](repeating: [Int](repeating: INF, count: MAXB+1), count: info.count)
+    
+    let a = info[0][0]
+    let b = info[0][1]
+    
+    // a가 훔칠 경우
+    if a < n {
+        D[0][0] = a
     }
     
-    D[0][0] = info[0][1] >= m ? info[0][0] : 0
-    D[0][1] = info[0][1] >= m ? 0 : info[0][1]
-
+    // b가 훔칠 경우
+    if b < m {
+        D[0][b] = 0
+    } 
+    
     for i in 1..<info.count {
+        let a = info[i][0]
+        let b = info[i][1]
         
-        if (D[i-1][1] + info[i][1]) >= m 
-        && (D[i-1][0] + info[i][0]) >= n { 
-            return -1 
+        for j in 0...MAXB {
+            if D[i-1][j] == INF { continue }
+            
+            // a를 선택
+            if D[i-1][j] + a < n {
+                D[i][j] = min(D[i][j], D[i-1][j] + a)    
+            }
+             
+            // b를 선택
+            if j + b < m {
+                D[i][j+b] = min(D[i][j+b], D[i-1][j])    
+            }
         }
-        
-        D[i][0] = (D[i-1][1] + info[i][1]) < m ? D[i-1][0] : D[i-1][0] + info[i][0]
-        D[i][1] = D[i][0] == D[i-1][0] ? D[i-1][1] + info[i][1] : D[i-1][1]
     }
     
-    return D[info.count-1][0]
+    var ans = INF
+    for j in 0..<m {
+        ans = min(ans, D[info.count-1][j])
+    }
+    
+    return ans == INF ? -1 : ans
 }
