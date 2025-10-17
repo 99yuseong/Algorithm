a33ae8ee2d1ee52054b3baa7216e9caade8e0b67
Nam Yuseong <dbtjd6511@gmail.com>
2025-10-17 12:30:47 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 129.53 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.swift
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.swift
@@ -22,59 +22,57 @@
 // 1. HH:mm:ss.sss -> ms 단위로 변환 24 * 60 * 60 * 1000 (8640만)
 // 2. 시작과 끝 지점 연산   S-T+1ms(시작시간)  S(완료시간) 
 
-
 // [완전탐색] - 10만 시간을 loop로 돌면서 찾는다? = O(1000억) (시간 초과)
 // [그리디]
 // [DP] - 8640만 배열 (공간 초과)
 
-// 9만 * 2000
-
-// 각 요소의 시작 시간 & 끝 시간을 포함한 좌우 1초 동안 포함되는 요소를 확인
-
 // [그리디]
 // 2000 * 2000 (포함되는 요소의 개수 확인) -> count max치면 가능
-
+    // 서버 증설 필요 개수가 변하는 시점은 서버 종료 + 1000 지점보다 다른 서버 시작이 있나?
+    // count ++
 
 
 func solution(_ lines:[String]) -> Int {
     
-    let FIN = 24 * 60 * 60 * 1000 - 1
-    
     // ms 단위로 변환
-    var lines = lines.map { $0.toTS() }.map { (max(0, $0.S - $0.T + 1), $0.S) }
+    var lines = lines
+        .map { $0.split(separator: " ").map { String($0) } }
+        .map { (S: $0[1].dateToMS(), T: $0[2].secondToMS()) } // (S, T)
+        .map { (start: $0.S - $0.T + 1, end: $0.S) } // (S - T + 1, S)
     
-    var maxCnt = 0
+    var maxCount = 0
     
-    for i in 0..<lines.count {
-    
-        var end = min(FIN, lines[i].1 + 999)
-        var cnt = 1
-        
-        for j in i+1..<lines.count {
-            if end >= lines[j].0 { cnt += 1 }
+    for i in 0..<lines.count { // O(2000)
+        var count = 1
+        for j in i+1..<lines.count { // O(2000)
+            if lines[i].end + 1000 > lines[j].start {
+                count += 1
+            }
         }
-        
-        maxCnt = max(maxCnt, cnt)
+        maxCount = max(maxCount, count)       
     }
     
-    return maxCnt
+    return maxCount
 }
 
 extension String {
-    func toTS() -> (S: Int, T: Int) {
+    
+    // HH:mm:ss.sss -> ms
+    func dateToMS() -> Int {
+        let comp = self.split(separator: ":")
         
-        let DTS = self.split(separator: " ").map { String($0) }
-        let S = Array(DTS[1]).map { String($0) } // HH:mm:ss.sss
-        let T = Array(DTS[2]).map { String($0) } // 0.000s
+        let H = Int(comp[0])!
+        let m = Int(comp[1])!
+        let sComp = comp[2].split(separator: ".")
+        let s = Int(sComp[0])!
+        let ms = Int(sComp[1])!
         
-        let H = Int(S[0])! * 10 + Int(S[1])!
-        let m = Int(S[3])! * 10 + Int(S[4])!
-        let s = Int(S[6])! * 10 + Int(S[7])!
-        let ms = Int(S[9])! * 100 + Int(S[10])! * 10 + Int(S[11])!
-        
-        let msS = ((H * 60 + m) * 60 + s) * 1000 + ms
-        let msT = Int(Double(Array(T[0..<T.count-1]).joined())! * 1000)
-        
-        return (S: msS, T: msT)
+        return ((H * 60 + m) * 60 + s) * 1000 + ms
+    }
+    
+    // 0.000s -> ms
+    func secondToMS() -> Int {
+        let comp = String(self.prefix(self.count-1))
+        return Int(Double(comp)! * 1000)
     }
 }
