0294f290a9c43c7836e1787f3f4bbedcfb5255a2
Nam Yuseong <dbtjd6511@gmail.com>
2025-10-22 21:28:55 +0900

    [level 4] Title: 지형 이동, Time: 664.01 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.swift
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.swift
@@ -1,106 +1,128 @@
 import Foundation
 
+// N*N 크기의 정사각형 지형
 // 숫자 = 높이
 
-// 모든 칸을 방문하는 탐험
+// 모든 칸을 탐험
+// 상하좌우 이동 - 높이차가 height이하
 
-// height 이하의 높이 차만 이동 가능
-
-// 사다리 설치 = 높이 차 비용
+// height 초과 시 사다리 사용 = 비용
 
 // return 사다리 설치 비용 최솟값
 
+// [제한사항]
 // N: 4~300
-// 9만칸
-// 높이: 1~1만
+// AREA: 16~9만
+// h: 1~1만
 
-// 1. BFS로 땅을 돌면서 영역을 마킹 - 1, 2, 3, 4 ... O(N^2)
-// 2. 전체 땅을 돌면서 O(N^2)
-    // 방문하지 않은 땅이라면
-    // 인접한 동일한 영역의 땅의 주변에서 1) 영역이 다르고, 2) height보다 차이가 큰 땅인경우 => 최소 차이를 등록
-    // 최소 비용 ++
+// 1. BFS로 돌면서 땅을 분리 (1,2,3...) - O(9만)
+    // 맞닿은 땅의 높이차가 height 이하 -> queue
+    // 맞닿은 땅의 높이차가 height 초과 -> [x1, y1, x2, y2, cost] push
 
-
-import Foundation
+// 2. 영역(정점) - 간선(높이차가 height 초과인 상하좌우) = cost가 최소인 MST
+    // 크루스칼 알고리즘    
+    // [x1, y1, x2, y2, cost]을 cost 순으로 정렬 - O(9만 * 4)
+    // union-find로 minCost 계산
 
 func solution(_ land:[[Int]], _ height:Int) -> Int {
+    
     let N = land.count
     let dx = [-1, 1, 0, 0]
     let dy = [0, 0, -1, 1]
     
-    // 1. BFS로 영역 구분
-    var area = Array(repeating: Array(repeating: 0, count: N), count: N)
-    var areaCount = 0
+    var coord = [[Int]]()
+    var visited = [[Int]](repeating: [Int](repeating: -1, count: N), count: N)
+    var curAREA = 0
     
     for i in 0..<N {
         for j in 0..<N {
-            if area[i][j] == 0 {
-                areaCount += 1
-                var queue = [[i, j]]
-                area[i][j] = areaCount
+            if visited[i][j] < 0 {
+                curAREA += 1
                 
-                var idx = 0
-                while idx < queue.count {
-                    let cur = queue[idx]
-                    idx += 1
+                var queue = [[Int]]()
+                queue.append([i,j])
+                visited[i][j] = curAREA
+                
+                while !queue.isEmpty {
+                    let cur = queue.removeFirst()
                     
                     for k in 0..<4 {
                         let x = cur[0] + dx[k]
                         let y = cur[1] + dy[k]
-                        guard 0..<N ~= x, 0..<N ~= y else { continue }
                         
-                        if area[x][y] == 0 && abs(land[cur[0]][cur[1]] - land[x][y]) <= height {
-                            area[x][y] = areaCount
-                            queue.append([x, y])
-                        }
+                        guard 0..<N ~= x && 0..<N ~= y else { continue }
+                        
+                        if visited[x][y] < 0 {
+                            let cost = abs(land[cur[0]][cur[1]] - land[x][y])
+                            if cost <= height {
+                                queue.append([x, y])
+                                visited[x][y] = curAREA
+                            } else {
+                                coord.append([cur[0], cur[1], x, y, cost])
+                            }
+                        } 
                     }
                 }
             }
         }
     }
     
-    // 2. 영역 간 간선 정보 수집
-    var edges: [(Int, Int, Int)] = [] // (cost, a, b)
-    for i in 0..<N {
-        for j in 0..<N {
-            for k in 0..<4 {
-                let x = i + dx[k]
-                let y = j + dy[k]
-                guard 0..<N ~= x, 0..<N ~= y else { continue }
-                
-                let a = area[i][j]
-                let b = area[x][y]
-                if a != b {
-                    let cost = abs(land[i][j] - land[x][y])
-                    edges.append((cost, a, b))
-                }
-            }
+    var parent = [Int](repeating: -1, count: curAREA+1)
+    
+    func find(_ a: Int) -> Int {
+        if parent[a] < 0 { return a }
+        parent[a] = find(parent[a])
+        return parent[a]
+    }
+    
+    func union(_ a: Int, _ b: Int) -> Bool {
+        var x = find(a)
+        var y = find(b)
+        
+        if x == y { return false }
+        
+        if parent[y] < parent[x] {
+            swap(&x, &y)
+        }
+        
+        if parent[x] == parent[y] {
+            parent[x] -= 1
+        }
+        parent[y] = x
+        return true
+    }
+    
+    var sorted = coord.sorted { $0[4] < $1[4] }
+    var minCost = 0
+    var latter = 0
+    
+    for c in sorted {
+        
+        let area1 = visited[c[0]][c[1]]
+        let area2 = visited[c[2]][c[3]]
+        
+        if union(area1, area2) {
+            minCost += c[4]
+            latter += 1
+        }
+        
+        if latter == curAREA-1 {
+            break
         }
     }
     
-    // 3. 간선을 비용 기준 정렬
-    edges.sort { $0.0 < $1.0 }
-    
-    // 4. Union-Find로 MST 구성
-    var parent = Array(0...areaCount)
-    func find(_ x: Int) -> Int {
-        if parent[x] != x {
-            parent[x] = find(parent[x])
-        }
-        return parent[x]
-    }
-    func union(_ a: Int, _ b: Int) {
-        let pa = find(a), pb = find(b)
-        if pa != pb { parent[pa] = pb }
-    }
-    
-    var totalCost = 0
-    for (cost, a, b) in edges {
-        if find(a) != find(b) {
-            union(a, b)
-            totalCost += cost
-        }
-    }
-    
-    return totalCost
+    return minCost
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
