010ab24034f83102c86ac801403703e7277833d3
Nam Yuseong <dbtjd6511@gmail.com>
2025-10-23 11:30:38 +0900

    [level 3] Title: 숫자 게임, Time: 383.78 ms, Memory: 25 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.swift
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.swift
@@ -1,83 +1,34 @@
 import Foundation
 
-// 숫자게임
-// A팀 B팀
-
-
-// 무작위 자연수
-// 딱 1번씩 경기
-// 숫자 큰 팀이 승리
-// 같다면 승점 없음
-
-// B팀이 출전 순서 정했을때, B팀이 얻는 승점
-
-// a: 출전 순서대로 나열된 배열
-// b: 부여받은 수 배열
-
-// return 최대 승점
-
-// a.count = 1~10만
-// 자연수 = 1~10억
-
-
-// [그리디]
-
-// 최대 승점 
-    // B에서 A보다 크고, 가장 작은 숫자를 배치
-    // 만약 큰 수가 없다면, 가장 작은 숫자를 배치
-
-// a.loop
-    // b.loop
-    // O(N^2) - (시간 초과)
-
-// O(N)으로 만들어야하는 구나. 착각했어
-// O(NlgN) or O(N)
-
-
-// 찾아야하는 건, a[i]보다 크면서 가장 작은 값. lowerbound
-// 없다면? -> 가장 작은 값
-
-
-// 정렬
-
-
 func solution(_ a:[Int], _ b:[Int]) -> Int {
     
-    var a = a.sorted { $0 > $1 }
-    var b = b.sorted { $0 > $1 }
+    // 이기려면 b는 a보다 크면서 최대한 작아야한다.
+    // 안된다면 b는 가장 작아야한다.
     
-    var winCount = 0
+    var a = a.sorted()
+    var b = b.sorted()
+    
+    [1, 3, 5, 7]
+    [2, 2, 6, 8]
+    
+    // b는 질때 가장 낮은 수로 지고, 이길때 가장 낮은 수로 이겨야한다.
+    
+    // b의 인원을 다 소모했을떄, a에게 이길 수 있는 횟수가 몇번인가
+    
+    var win = 0
+    
+    var aIdx = 0
     var bIdx = 0
     
-    for target in a {
-        if b[bIdx] > target {
-            winCount += 1
+    while bIdx < b.count {
+        if b[bIdx] > a[aIdx] {
+            win += 1
+            bIdx += 1
+            aIdx += 1
+        } else {
             bIdx += 1
         }
     }
     
-    return winCount
+    return win
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
