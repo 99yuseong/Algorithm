046a5d8505143420efafa9697aa0bb0b605c260d
Nam Yuseong <dbtjd6511@gmail.com>
2025-11-12 15:01:45 +0900

    [Gold V] Title: LCS, Time: 24 ms, Memory: 76984 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.swift
+++ 백준/Gold/9251. LCS/LCS.swift
@@ -1,23 +1,46 @@
-// 0.1초
 
-// 최장 공통부분수열
-// 최대 천글자
+// LCS 최장공통부분 수열
+// 부분 수열 중 가장 긴 것
 
-// D[i][j]: A의 i번째, B의 j번째까지의 최대 LCS의 길이
-// D[i][j] = D[i-1][j-1] + 1 if A[i] == B[j]
-// D[i][j] = max(D[i][j-1], D[i-1][j]) if A[i] != B[j]
+// ACAYKP
+// CAPCAK
 
-let A = ["-"] + Array(readLine()!)
-let B = ["-"] + Array(readLine()!)
-var D = Array(repeating: Array(repeating: 0, count: B.count), count: A.count)
+// ACAK
+// 최대 1000글자
 
-for i in 1...A.count-1 {
-    for j in 1...B.count-1 {
-        if A[i] == B[j] {
-            D[i][j] = D[i-1][j-1] + 1
-        } else {
-            D[i][j] = max(D[i-1][j], D[i][j-1])
+// 최장공통부분 수열을 찾으려면, 결국 모든 지점에서 시작하는 부분을 비교해야한다. 이 개념
+// 2^1000으로 시간 초과
+
+// DP로 푼다고 하면
+// A의 i번째, B의 j번째의 LCS를 저장
+
+// DP[i][j] : A의 i번째, B의 j번째까지 인덱스까지의 LCS
+// DP[i][j] = DP[i-1][j-1] + 1 (if A[i] == B[j])
+// DP[i][j] = max(DP[i][j-1], DP[i-1][j] (if A[i] != B[j])
+
+func solution(_ A: [Character], _ B: [Character]) -> Int {
+    
+    var DP = [[Int]](repeating: [Int](repeating: 0, count: B.count+1), count: A.count+1)
+    
+    DP[0][0] = 0
+    DP[1][0] = 0
+    DP[0][1] = 0
+    
+    for i in 1...A.count {
+        for j in 1...B.count {
+            
+            if A[i-1] == B[j-1] {
+                DP[i][j] = DP[i-1][j-1] + 1
+            } else {
+                DP[i][j] = max(DP[i-1][j], DP[i][j-1])
+            }
         }
     }
+    
+    return DP[A.count][B.count]
 }
-print(D[A.count-1][B.count-1])
+
+let stringA = Array(readLine()!)
+let stringB = Array(readLine()!)
+print(solution(stringA, stringB))
+
