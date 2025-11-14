28eb66d1286a8707598e04fa95477f0ae62dc94e
Nam Yuseong <dbtjd6511@gmail.com>
2025-11-14 13:52:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 180 ms, Memory: 148292 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.swift
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.swift
@@ -1,28 +1,52 @@
-let NK = readLine()!.split(separator: " ").map { Int($0)! }
-let N = NK[0] // N 물품의 수, 1~100개
-let K = NK[1] // K 최대 무게, 1~10만
+// 배낭
+// N개의 물건
+// 각 물건은 W무게와 V의 가치
+// 최대 K만큼의 무게
+// return V의 합의 최댓값
 
-// 물품당 무게 1~10만, 가치 0~1000
-var WV: [[Int]] = [[0,0]]
-for _ in 0..<N {
-    WV.append(readLine()!.split(separator: " ").map { Int($0)! })
-}
+// N: 1~100개 물건
+// K: 1~10만의 무게
+// W: 1~10만
+// V: 0~1천
 
-// D[i][j]: 최대 무게가 i일때, j번째까지 물건을 고려한 최대 가치
-// D[i][j] = D[i-1][j] // 무거워서 못담았을 때
-// D[i][j] = D[i-WV[j][0]][j-1] + WV[j][1]
+// 이번 물건을 선택하는 게 다음 물건의 선택에 영향을 준다.
+// DP
 
+// DP[i][j]: i번째 물건을 고려하면서 j의 무게를 견딜 수 있을 때 얻는 최대 가치
 
-var D = Array(repeating: Array(repeating: 0, count: N+1), count: K+1)
+// DP[i][j] = max(DP[i-1][j-products[i][0]] + products[i][1], DP[i-1][j])
+// 이때, j >= products[i][0]
 
-for i in 1...K {
-    for j in 1...N {
-        if i >= WV[j][0] { // 넣을 수 있을 때
-            D[i][j] = max(D[i-WV[j][0]][j-1] + WV[j][1], D[i][j-1])
-        } else {
-            D[i][j] = D[i][j-1]
+// DP[0][k] = 0
+
+func solution(_ k: Int, _ products: [[Int]]) -> Int {
+
+    let n = products.count
+    var DP = [[Int]](repeating: [Int](repeating: 0, count: k+1), count: n+1)
+    
+    for i in 1...n {
+        let product = products[i-1]
+        let weight = product[0]
+        let value = product[1]
+        
+        for j in 1...k {
+            if j >= weight {
+                DP[i][j] = max(DP[i-1][j-weight] + value, DP[i-1][j])
+            } else {
+                DP[i][j] = DP[i-1][j]
+            }
         }
     }
+    
+    return DP[n][k]
 }
 
-print(D[K][N])
+let NK = readLine()!.split(separator: " ").map { Int(String($0))! }
+let N = NK[0]
+let K = NK[1]
+var WVs: [[Int]] = []
+for _ in 0..<N {
+    let WV = readLine()!.split(separator: " ").map { Int(String($0))! }
+    WVs.append(WV)
+}
+print(solution(K, WVs))
