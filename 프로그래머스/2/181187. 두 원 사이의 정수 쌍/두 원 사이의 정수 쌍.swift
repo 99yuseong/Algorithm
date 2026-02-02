0f7b48b2806abff9cd6d4da640a32672db87d50a
Nam Yuseong <dbtjd6511@gmail.com>
2026-02-02 20:49:47 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 34.99 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.swift
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.swift
@@ -1,39 +1,39 @@
 import Foundation
 
 func solution(_ r1:Int, _ r2:Int) -> Int64 {
-    print(count(5))
-    print(countAtLine(5))
-    return count(r2) - count(r1) + countAtLine(r1)
+    return countInCircle(r2) - countInCircle(r1) + countAtLine(r1)
 }
 
-func count(_ r: Int) -> Int64 {
+func countInCircle(_ r: Int) -> Int64 {
     
     if r == 1 { return 5 }
     
-    let cnt: Int64 = Int64(r) * 2 + 1 + 2
-    var side: Int64 = 0
+    let center = r * 2 + 1
+    var side = 0
     
     for i in 1...r-1 {
         var n2 = Double(r * r) - Double(i * i)
-        side += 2 * Int64(sqrt(n2)) + 1
+        side += 2 * Int(sqrt(n2)) + 1
     }
+    side += 1
     
-    return cnt + side * 2
+    return Int64(center + side * 2)
 }
 
 func countAtLine(_ r: Int) -> Int64 {
 
     if r == 1 { return 4 }
     
-    var cnt: Int64 = 4
-    var side: Int64 = 0
+    var center = 2
+    var side = 0
     
     for i in 1...r-1 {
         let n2 = Double(r * r) - Double(i * i)
-        var n = round(sqrt(n2))
-        if n2 == n * n { side += 2 }
+        var n = Int(sqrt(n2))
+        if Int(n2) == n * n { side += 2 }
     }
+    side += 1
     
-    return cnt + side * 2
+    return Int64(center + side * 2)
     
 }
