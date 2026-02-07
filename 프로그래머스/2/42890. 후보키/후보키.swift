724368051c78f6f22c18af2f0194d495eea20eea
Nam Yuseong <dbtjd6511@gmail.com>
2026-02-07 13:46:03 +0900

    [level 2] Title: 후보키, Time: 8.81 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.swift
+++ 프로그래머스/2/42890. 후보키/후보키.swift
@@ -1,137 +1,52 @@
 import Foundation
 
-// 후보키
-// 유일하게 식별, 최소성
+// c - 1~8개의 항목이 있다.
+// r - 1~10개의 줄이 들어온다.
+// 각 문자열 항의 길이는 1~8의 소문자와 숫자다.
 
-// r: 1~20 (튜플)
-// c: 1~8
-// 문자열 길이는 1~8 (소문자와 숫자)
+// 모두 유일하게 식별 가능하다! / 아니라면 Set으로 처리해야하고
 
-// 20 * 8 * 6 ~~ 1000개 데이터
+// 유일성과 최소성을 만족시켜야한다.
 
-// 유일성은 Dictionary 같은데, 
-// 최소성이 문제네
+// 1. 유일성 - 각 Key로 변환한 값의 개수가 r의 개수와 같아야한다.
+// 2. 최소성 - 가장 작은 수로 구성된 Key부터 진행하면서, 최종 결과에 현재 Key가 포함되었는 지 확인해야한다.
 
-// 4 + 6 + 4 + 1 > 15개의 조합키
-// 유일한가 > 인원 수 
-// 최소인가 > set -> 요소를 contain하는가?
-
-// 2^4 > 
-// combi
-
-// 조합으로 학번, 이름, 전공, 학년을 키로 설정 
-
-import Foundation
-
-func solution(_ relation: [[String]]) -> Int {
-    let rowCount = relation.count
-    let colCount = relation[0].count
-    var candidateKeys: [Set<Int>] = []
+func solution(_ relation:[[String]]) -> Int {
     
-    // 1. 모든 컬럼 조합을 비트마스킹으로 탐색 (1부터 2^colCount - 1까지)
-    // 예: 컬럼이 3개면 1(001) ~ 7(111)
-    for i in 1..<(1 << colCount) {
-        var currentCombination = Set<Int>()
-        for j in 0..<colCount {
-            if (i & (1 << j)) != 0 {
-                currentCombination.insert(j)
+    let c = relation[0].count
+    let r = relation.count
+    
+    var uniqueKey: [Set<Int>] = []
+    var minimumKey: [Set<Int>] = []
+    
+    // 1. 각 컬럼의 조합을 모두 돌면서 확인 - 2^8 = 256개
+    // c를 조합으로 확인
+    for i in 1..<(1 << c) {
+        
+        // 0. 조합 선택
+        var curCombi: [Int] = []
+        for j in 0..<c {
+            if i & (1 << j) != 0 {
+                curCombi.append(j)
             }
         }
         
-        // 2. 최소성 검사: 이미 등록된 후보키가 현재 조합의 부분집합인지 확인
-        if candidateKeys.contains(where: { $0.isSubset(of: currentCombination) }) {
-            continue
+        // 1. 유일성 ex) [0, 1, 3]
+        let uniques: [String] = relation.map { r in
+            curCombi.map { r[$0] }.joined(separator: "/")
         }
+        guard r == Set(uniques).count else { continue }
         
-        // 3. 유일성 검사
-        var set = Set<String>()
-        for row in relation {
-            let keyString = currentCombination.map { row[$0] }.joined(separator: "-")
-            set.insert(keyString)
-        }
-        
-        if set.count == rowCount {
-            candidateKeys.append(currentCombination)
-        }
+        uniqueKey.append(Set(curCombi))
     }
     
-    return candidateKeys.count
-}
-
-// func solution(_ relation:[[String]]) -> Int {
     
-//     var ans: Set<[Key]> = []
-    
-//     for i in (1...4) {
-//         out: for key in combi(i) {
-            
-//             // key: [Key]가 조합키가 가능한가?
-            
-//             // 1. 유일성 보장
-//             let uniques = Set(relation.map { r in key.map { r[$0.rawValue] }.reduce("", +) })
-//             guard relation.count == uniques.count else { continue }
-            
-            
-//             // 2. 최소성 보장
-//             if ans.contains(where: { Set($0).isSubset(of: Set(key)) }) { continue }
-                
-//             ans.insert(key)
-//         }
-//     }
-    
-//     return ans.count
-// }
-
-func 부분집합(_ keys: [Key]) -> Set<[Key]> {
-    
-    var ans: Set<[Key]> = []
-    var arr: [Key] = []
-    
-    func select(_ k: Int) {
-        
-        if k == keys.count {
-            if arr.isEmpty { return }
-            ans.insert(arr)
-            return
-        }
-        
-        arr.append(keys[k])
-        select(k+1)
-        arr.removeLast()
-        
-        select(k+1)
+    // 2. 최소성
+    uniqueKey.sort { $0.count < $1.count }
+    for key in uniqueKey {
+        if minimumKey.contains { $0.isSubset(of: key) } { continue }
+        minimumKey.append(key)
     }
-    select(0)
     
-    return ans
-}
-
-enum Key: Int, CaseIterable {
-    case studentNum = 0
-    case name = 1
-    case major = 2
-    case year = 3
-}
-
-func combi(_ n: Int) -> [[Key]] {
-    
-    var selection: [[Key]] = []
-    var arr: [Key] = []
-    
-    func select(_ k: Int, _ s: Int) {
-        
-        if k == n {
-            selection.append(arr)
-            return 
-        }
-        
-        for i in (s..<4) {
-            arr.append(Key.allCases[i])
-            select(k+1, i+1)
-            arr.removeLast()
-        }
-    }
-    select(0, 0)
-    
-    return selection
+    return minimumKey.count
 }
