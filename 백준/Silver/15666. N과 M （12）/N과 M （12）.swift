15ec70b260a08ad6dcb391416a78c49c1ad43ff9
Nam Yuseong <dbtjd6511@gmail.com>
2026-02-23 09:52:47 +0900

    [Silver II] Title: N과 M (12), Time: 12 ms, Memory: 69564 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.swift
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.swift
@@ -1,26 +1,44 @@
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-var arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+// N과 M이 주어졌을 때, 길이가 M인 수열을 모두 작성
 
-let N = input[0]
-let M = input[1]
-
-var ans = Array(repeating: 0, count: M)
-var check = Set<[Int]>()
-
-func box(_ k: Int) { // 현재까지 k개의 수를 택함
-    if k == M {
-        if !check.contains(ans) {
-            check.insert(ans)
-            print(ans.map { String($0) }.joined(separator: " "))
-        }
-        return
-    }
+func solution(_ arr: [Int], _ M: Int) -> [[Int]] {
+    // arr의 요소 중 M개를 고른 수열
+    // 출력은 사전에서 증가하는 순
     
-    for i in 0..<N {
-        if k == 0 || ans[k-1] <= arr[i] {
-            ans[k] = arr[i]
-            box(k+1)
+    // 요소를 유일하게 만든뒤 중복조합
+    func repeatedCombi(_ arr: [Int], _ r: Int) -> [[Int]] {
+
+        let arr = Array(Set(arr)).sorted()
+
+        var result: [[Int]] = []
+        var cur: [Int] = []
+        
+        func select(_ k: Int, _ s: Int) {
+            
+            if k == r {
+                result.append(cur)
+                return
+            }
+            
+            for i in s..<arr.count {
+                cur.append(arr[i])
+                select(k+1, i)
+                cur.removeLast()
+            }
         }
+        select(0, 0)
+        return result
     }
+     
+    return repeatedCombi(arr, M)
 }
-box(0)
+
+let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+let N = NM[0]
+let M = NM[1]
+let A = readLine()!.split(separator: " ").map { Int(String($0))! }
+var ANS = ""
+for r in solution(A, M) {
+    ANS += r.map { String($0) }.joined(separator: " ")
+    ANS += "\n" 
+}
+print(ANS)
