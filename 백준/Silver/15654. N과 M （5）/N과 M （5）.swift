72b4be5590747662b65d646ea3ebef469a468ef6
Nam Yuseong <dbtjd6511@gmail.com>
2026-02-23 09:09:02 +0900

    [Silver III] Title: N과 M (5), Time: 92 ms, Memory: 78268 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.swift
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.swift
@@ -1,24 +1,47 @@
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let N = input[0]
-let M = input[1]
+// N과 M이 주어졌을 때, 길이가 M인 수열을 모두 작성
 
-var arr: [Int] = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
-var box = Array(repeating: 0, count: M)
-var isUsed = Array(repeating: false, count: N)
+func solution(_ arr: [Int], _ M: Int) -> [[Int]] {
+    // arr의 요소 중 M개를 고른 수열
+    // 출력은 사전에서 증가하는 순
+    // 순열
+    
+    func permu(_ arr: [Int], _ r: Int) -> [[Int]] {
+        let arr = arr.sorted()
 
-func solution(_ k: Int) {
-    if k == M {
-        print(box.map { String($0) }.joined(separator: " "))
-        return
+        var result: [[Int]] = []
+        var cur: [Int] = []
+        var isUsed = [Bool](repeating: false, count: arr.count)
+        
+        func select(_ k: Int) {
+            if k == r {
+                result.append(cur)
+                return
+            }
+            
+            for i in 0..<arr.count {
+                if !isUsed[i] {
+                    cur.append(arr[i])
+                    isUsed[i] = true
+                    select(k+1)
+                    isUsed[i] = false
+                    cur.removeLast()
+                }
+            }
+        }
+        select(0)
+        return result
     }
     
-    for i in 0..<N {
-        if !isUsed[i] {
-            isUsed[i] = true
-            box[k] = arr[i]
-            solution(k+1)
-            isUsed[i] = false
-        }
-    }
+    return permu(arr, M)
 }
-solution(0)
+
+let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+let N = NM[0]
+let M = NM[1]
+let A = readLine()!.split(separator: " ").map { Int(String($0))! }
+var ANS = ""
+for r in solution(A, M) {
+    ANS += r.map { String($0) }.joined(separator: " ")
+    ANS += "\n" 
+}
+print(ANS)
