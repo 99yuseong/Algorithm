76c985508a65b8ea135d3fb19008c657079a4d1c
Nam Yuseong <dbtjd6511@gmail.com>
2026-03-02 10:26:17 +0900

    [level 3] Title: 네트워크, Time: 0.58 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -1,65 +1,61 @@
-// A - B - C = 같은 네트워크
+import Foundation
 
-// n: 컴퓨터의 개수
-// 연결정보
+// 네트워크 A-B B-C
 
-// 네트워크의 개수를 리턴
+// n개의 컴퓨터, 네트워크의 개수는 몇개?
 
 // n: 1~200
-// 0~n-1 인덱스의 컴퓨터
-// [i][j] = 1
+// BFS/DFS로 섬의 개수를 구하라
 
-// 1. [i][j] -> graph로 변환 O(n^2)
-// 2. BFS -> O(n)
-    // 0~N-1까지 visited == false에서 BFS
+// 1. graph 변환 > 4만
+// 2. BFS > 4만
 
-import Foundation
+struct Queue<T> {
+    var inbox: [T] = []
+    var outbox: [T] = []
+    var isEmpty: Bool { return inbox.isEmpty && outbox.isEmpty }
+    mutating func append(_ k: T) { inbox.append(k) }
+    mutating func removeFirst() -> T? {
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        return outbox.popLast()
+    }
+}
 
 func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     
-    let N = computers.count
-    
-    // 1. graph로 변환 O(N^2)
     var graph: [Int: [Int]] = [:]
-    
-    for i in 0..<N {
-        for j in 0..<N {
-            guard i != j else { continue }
-            guard computers[i][j] == 1 else { continue }
-            
-            graph[i, default: []].append(j)   
-        }
-    }
-    
-    // 2. BFS - O(N)
-    var networkCnt = 0
-    var visited = [Bool](repeating: false, count: N)
-    
-    func BFS(_ start: Int) {
-        var queue: [Int] = []
-        
-        queue.append(start)
-        visited[start] = true
-        
-        while !queue.isEmpty {
-            let cur = queue.removeFirst()
-            
-            for node in graph[cur] ?? [] {
-                guard !visited[node] else { continue }
-                
-                queue.append(node)
-                visited[node] = true
+    for i in 0..<n {
+        for j in 0..<n {
+            if i == j { continue }
+            if computers[i][j] == 1 {
+                graph[i, default: []].append(j)    
             }
         }
-        
-        // 연결된 모든 노드 방문 후 cnt++
-        networkCnt += 1
     }
     
-    for i in 0..<N {
-        guard !visited[i] else { continue }
-        BFS(i)
+    var visited = [Bool](repeating: false, count: n)
+    var networks = 0
+    
+    for i in 0..<n {
+        if !visited[i] {
+            var queue = Queue<Int>()
+            queue.append(i)
+            networks += 1
+            
+            while !queue.isEmpty {
+                let cur = queue.removeFirst()!
+                for v in graph[cur, default: []] {
+                    if !visited[v] {
+                        queue.append(v)            
+                        visited[v] = true
+                    }
+                }
+            }
+        }
     }
     
-    return networkCnt
+    return networks
 }
