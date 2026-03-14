cf19e565979d781cb2ddd45706a3c85af4a4e1c4
Nam Yuseong <dbtjd6511@gmail.com>
2026-03-14 17:29:20 +0900

    [level 2] Title: 전화번호 목록, Time: 223.39 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,62 +1,24 @@
-# O(NlgN)안으로
-# 이분탐색? -> 정렬 후, N개 배열을 돌면서 이분탐색으로 접두사 여부 확인
-    # 1. 배열 정렬
-    # 2. for문을 돌면서:
-        # target의 길이만큼 mid를 자르고
-        # 크기 비교 후 이분탐색
-        
+# 접두어
+# 1개라도 존재하면 리턴
+
+# 1~100만개의 번호
+# 20자리
+
+# 그리디
+# 정렬 > 앞뒤 확인
+
 def solution(phone_book):
-    phone_dict = {}
     
-    for phone in phone_book:
-        phone_dict[phone] = 1
+    phone_book.sort()
     
-    for phone in phone_book:
-        temp = ""
-        for i in range(len(phone)):
-            temp += phone[i]
-            
-            if temp in phone_dict and temp != phone:
-                return False
-            
+    for i in range(len(phone_book)):
+        
+        p = phone_book[i]
+        
+        if i > 0 and len(p) <= len(phone_book[i-1]) and phone_book[i-1][:len(p)] == phone_book[i]:
+            return False
+        
+        if i+1 < len(phone_book) and len(p) <= len(phone_book[i+1]) and phone_book[i+1][:len(p)] == phone_book[i]:
+            return False
+        
     return True
-        
-# def solution(phone_book):
-    
-#     n = len(phone_book)    
-#     sorted_phone_book = sorted(phone_book)
-        
-#     for i in range(n):
-        
-#         l = i
-#         r = n - 1
-        
-#         while l <= r:
-            
-#             m = (l + r) // 2
-            
-#             if idx != m and sorted_phone_book[m].startswith(sorted_phone_book[idx]): 
-#                 return False
-            
-#             if sorted_phone_book[m] < sorted_phone_book[idx]:
-#                 l = m + 1
-#             else:
-#                 r = m - 1
-    
-#     return answer
-
-# 전화번호 중 다른 번호의 접두어가 있는지?
-
-# 전화번호 배열 -> 접두사인 경우가 있으면 false / 있으면 true
-
-# 배열 1~100만
-# 각 아이템 길이는 1~20
-# 중복은 없음
-
-# 119 (접두사)
-
-# 숫자 개념
-
-
-
-# 해시 -> 뭐로 시작하는 수 - 해시개념인가?
