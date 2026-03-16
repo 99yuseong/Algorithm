85459094bd555b38d7f9ba5c75828a0486b02156
Nam Yuseong <dbtjd6511@gmail.com>
2026-03-16 11:08:42 +0900

    [level 2] Title: 프로세스, Time: 1.57 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,26 +1,40 @@
+# 운영체제의 역할
+
+# 특정 프로세스는 몇번째 실행?
+
+# 우선순위가 높은 순서대로 실행
+
+# 근데 순서가 있고 순회를 한다.
+# 앞에서 빼고 뒤로 넣는 구조
+
+# n: 1~100
+# 숫자가 클수록 우선순위가 높다.
+
+# 인덱스를 추적해야 한다..
+
 from collections import deque
 
 def solution(priorities, location):
 
-    dq = deque(priorities)
-    target = location
-    execute_order = 1
+    n = len(priorities)
+    queue = deque()
     
-    while dq:
-        cur_p = dq[0]
+    for i in range(n):
+        queue.append([priorities[i], i])        
+    
+    for i in range(1, n+1):
         
-        if any(cur_p < x for x in list(dq)[1:]):
-            dq.append(dq.popleft())
-            if target == 0:
-                target = len(dq) - 1
-            else:
-                target -= 1
-        else:
-            dq.popleft()
-            if target == 0:
-                return execute_order
-            execute_order += 1
-            target -= 1
-    
-    return execute_order
-    
+        m = -1
+        for p, _ in queue:
+            m = max(m, p)
+        
+        while queue[0][0] != m:
+            queue.append(queue.popleft())
+        
+        q = queue.popleft()
+        
+        if q[1] == location:
+            return i
+        
+    answer = 1    
+    return answer
