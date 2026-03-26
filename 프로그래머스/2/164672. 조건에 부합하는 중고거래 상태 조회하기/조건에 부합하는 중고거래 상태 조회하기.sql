27368a776beeedcec12076ffb4eab691b7c29cdf
Nam Yuseong <dbtjd6511@gmail.com>
2026-03-26 11:41:09 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,18 @@
+# 중고거래 게시판
+
+# 2022년 10월 5일 등록
+# STATUS > 단어 변환 switch
+
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    CASE STATUS
+        WHEN 'SALE' THEN '판매중'
+        WHEN 'RESERVED' THEN '예약중'
+        ELSE '거래완료'
+    END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE >= '2022-10-05' AND CREATED_DATE < '2022-10-06'
+ORDER BY BOARD_ID DESC
