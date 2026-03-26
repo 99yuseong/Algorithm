dfbbd1d3acc783f02b0dc1bb42959054d617b19c
Nam Yuseong <dbtjd6511@gmail.com>
2026-03-26 11:49:41 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 1, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/299307. 대장균의 크기에 따라 분류하기 1/대장균의 크기에 따라 분류하기 1.sql
@@ -0,0 +1,12 @@
+# 대장균 정보
+# 크기에따라 SIZE
+
+SELECT
+    ID,
+    CASE
+        WHEN SIZE_OF_COLONY > 1000 THEN 'HIGH'
+        WHEN SIZE_OF_COLONY > 100 THEN 'MEDIUM'
+        ELSE 'LOW'
+    END AS SIZE
+FROM ECOLI_DATA
+ORDER BY ID
