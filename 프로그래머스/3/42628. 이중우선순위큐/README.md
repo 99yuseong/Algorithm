a2362e9cd8a2e4f8f2c16130044882ab2edae354
Nam Yuseong <dbtjd6511@gmail.com>
2026-03-31 10:23:21 +0900

    [level 3] Title: 이중우선순위큐, Time: 99.61 ms, Memory: 23.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=python3) 
 
 ### 성능 요약
 
-메모리: 21 MB, 시간: 576.49 ms
+메모리: 23.2 MB, 시간: 99.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 24일 11:44:52
+2026년 03월 31일 10:23:19
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,47 @@
+# 이중 우선순위 큐
+# 모든 연산 후 큐가 비어있으면 [0,0] 아니면 [최대, 최소]
+
+# 100만 cmd
+# 빈큐 삭제 명령은 무시
+
+# 최대힙, 최소힙 따로?
+# 숫자는 중복될 수 있다. 빠짐 체크가 애매하긴함. > id를 사용하자
+
+import heapq
+
+def solution(operations):
+    
+    min_heap = []
+    max_heap = []
+    removed_set = set()
+    
+    id = 1
+    for op in operations:
+        cmd, num = op.split()
+        num = int(num)
+        
+        if cmd == 'I': # 큐에 추가
+            heapq.heappush(min_heap, (num, id))
+            heapq.heappush(max_heap, (-num, id))
+            id += 1
+        
+        elif num == 1: # 최댓값 삭제
+            while max_heap and max_heap[0][1] in removed_set:
+                heapq.heappop(max_heap)
+            if max_heap:
+                removed_set.add(heapq.heappop(max_heap)[1])
+        
+        else: # 최솟값 삭제
+            while min_heap and min_heap[0][1] in removed_set:
+                heapq.heappop(min_heap)
+            if min_heap:
+                removed_set.add(heapq.heappop(min_heap)[1])
+        
+    
+    while max_heap and max_heap[0][1] in removed_set:
+        heapq.heappop(max_heap)
+    
+    while min_heap and min_heap[0][1] in removed_set:
+        heapq.heappop(min_heap)
+    
+    return [-max_heap[0][0], min_heap[0][0]] if min_heap and max_heap else [0,0]
