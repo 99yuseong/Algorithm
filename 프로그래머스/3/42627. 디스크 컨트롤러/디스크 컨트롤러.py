d914eafd14bb7a56dc792ea6cf9c7a9f3956b56e
Nam Yuseong <dbtjd6511@gmail.com>
2026-04-02 11:37:31 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 56.12 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,58 +1,51 @@
-# 하드디스크는 한 번에 하나의 작업만
-
+# 하드디스크 한번에 하나의 작업만
 # 우선순위 디스크 컨트롤러
-# 작업 요청 > 번호, 요청시각, 소요시간 대기큐
 
-# 우선 순위 1. 소요시간이 짧을 수록 2. 요청 시각이 빠를 수록 3. 번호가 작을 수록
+# 디스크 컨트롤러 (작업번호, 요청시각, 소요시간)
+# 하드디스크 - 작업X, 대기큐O > 작업
+# wk_time 짧을 수록, rq_time이 빠를 수록, wk_id가 작을수록 빠르게
 
+# 한번에 1개의 작업만 가능
 
-# 1. 작업 종료 확인
-# 2. 작업 요청 확인
-# 3. 대기큐 추가
-# 4. (작업중) continue
-# 5. (작업완료) 대기큐 확인 > 우선순위 높은 것 시작
+# 반환 시간 > 종료 - 요청시간
 
-# 평균 반환 시간의 정수부분을 리턴
-
-# jobs: 500
-# 1000초 > 최대 50만초
-
-
-# O(t + NlogN)
-# loop t를 카운트 
-    # 작업 요청 확인 > 대기큐 추가
-    # 작업 종료 확인 > (작업완료시) 우선순위 높은 것부터 시작
+# 매초 확인해야함
+# jobs는 최대 500개
+# 500개 * 1000초 = 약 50만초 + 1000
 
 import heapq
 
 def solution(jobs):
     
+    # 반환시간
     n = len(jobs)
+    rt_time = []
     
-    # jobs[i] = [request_time, work_time]
-    # 번호는 정렬 전 인덱스로 관리
-    indexed_jobs = [(rq, wt, i) for i, (rq, wt) in enumerate(jobs)]
-    indexed_jobs.sort(key=lambda x: x[0])  # 요청 시각 순
+    jobs.sort(key=lambda x: x[0])
     
-    heap = []
-    time = 0
-    i = 0
-    total_rt = 0
+    # 매초 확인
+    t = 0
+    cur_wk_idx = 0
+    disk = [] # (fn_time, rq_time)
+    controller = [] # (wk_time, rq_time)
     
-    while i < n or heap:
-        # 현재 시각까지 들어온 작업 전부 대기큐에 넣기
-        while i < n and indexed_jobs[i][0] <= time:
-            rq, wt, idx = indexed_jobs[i]
-            # 우선순위: 소요시간, 요청시각, 번호
-            heapq.heappush(heap, (wt, rq, idx))
-            i += 1
+    while len(rt_time) < n:
         
-        if heap:
-            wt, rq, idx = heapq.heappop(heap)
-            time += wt
-            total_rt += time - rq
-        else:
-            # 대기 작업이 없으면 다음 요청 시각으로 점프
-            time = indexed_jobs[i][0]
-    
-    return total_rt // n
+        while cur_wk_idx < n and jobs[cur_wk_idx][0] == t:
+            rq_time, wk_time = jobs[cur_wk_idx]
+            heapq.heappush(controller, (wk_time, rq_time, cur_wk_idx))
+            cur_wk_idx += 1
+            
+        if len(disk) == 0 or disk[-1][0] == t:
+            
+            if len(disk) > 0: 
+                disk.pop()
+            
+            if controller:
+                wk_time, rq_time, _ = heapq.heappop(controller)
+                disk.append((t+wk_time, rq_time))
+                rt_time.append(t+wk_time-rq_time)
+        
+        t += 1
+        
+    return sum(rt_time) // len(rt_time)
