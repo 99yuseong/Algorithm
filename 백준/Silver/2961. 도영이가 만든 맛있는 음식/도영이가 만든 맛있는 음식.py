389569f3d5ad7b1a4c6e9ea18281af05b391f7d7
Nam Yuseong <dbtjd6511@gmail.com>
2026-04-17 16:12:32 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,14 +1,26 @@
-# 재료가 N개
-# 신맛과 쓴맛
-# 적어도 1개 이상 / 신맛과 쓴맛의 차이를 작게
-# 2^N개에 대해서 > 최소차이를 갱신
+# N개의 재료
+# S맛과 B맛
+# S는 곱연산, B는 합연산
+
+# 적절히 추가해서 S와 B의 차이를 최소로
+# 적어도 1개 이상 사용할것
+
+# 가장 S - B가 작은 요리
+
+# 각 재료를 넣을지 말지 결정 > 2^N
+# nC1 + nC2 + nC3 + ... + nCn을 모두 순회
+# 2^N * k
+
+# 비트마스킹을 활용
 
 def solution(SB):
-    
+
     N = len(SB)
     min_diff = float('inf')
-    
-    for mask in range(1, 1 << N):
+
+    for mask in range(1 << N):
+        if mask == 0: 
+            continue # 적어도 1개 이상을 선택
         
         S = 1
         B = 0
@@ -20,7 +32,7 @@ def solution(SB):
 
         if abs(S-B) < min_diff:
             min_diff = abs(S-B)
-
+    
     return min_diff
 
 def main():
