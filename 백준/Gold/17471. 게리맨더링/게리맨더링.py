2a938e8cf8fd0c10dcaeade90fd602a87c90912d
Nam Yuseong <dbtjd6511@gmail.com>
2026-04-18 23:32:38 +0900

    [Gold III] Title: 게리맨더링, Time: 136 ms, Memory: 114364 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.py
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.py
@@ -1,49 +1,54 @@
 from collections import deque
 
-def bfs(group_mask, graph, N):
+def bfs(mask, graph, N):
+    
     queue = deque()
-
-    for i in range(N):
-        if group_mask & (1 << i):
+    
+    for i in range(1, N+1):
+        if mask & (1 << i):
             queue.append(i)
-            group_mask &= ~(1 << i)
+            mask &= ~(1 << i)
             break
 
     while queue:
         cur = queue.popleft()
-
+        
         for near in graph[cur]:
-            if group_mask & (1 << near):
+            if mask & (1 << near):
                 queue.append(near)
-                group_mask &= ~(1 << near)
-
-    return group_mask == 0
+                mask &= ~(1 << near)
+    
+    return mask == 0
+    
 
 def solution(P, A):
+    # mask로 group을 2개로 나눌거임
+    # 나누고 BFS로 각각 이어짐을 확인
+    # 인구수 비교 갱신
     N = len(P)
-
-    # 0-index 그래프
-    graph = [[] for _ in range(N)]
+    graph = [[] for _ in range(N+1)]
+    
     for i in range(N):
-        for near in A[i][1:]:
-            graph[i].append(near - 1)
-
+        graph[i+1] = A[i][1:]
+        
     total = sum(P)
     min_diff = float('inf')
-    full = (1 << N) - 1
-
-    for mask in range(1, (1 << N) - 1):
-        other = full ^ mask
-
+    
+    for mask in range(1 << 1, (1 << (N+1)) - 1):
+        
+        full = (1 << (N+1)) - 2
+        other = mask ^ full
+        
         if bfs(mask, graph, N) and bfs(other, graph, N):
             p = 0
-            for i in range(N):
+            
+            for i in range(1, N+1):
                 if mask & (1 << i):
-                    p += P[i]
-
-            min_diff = min(min_diff, abs(total - 2 * p))
-
-    return -1 if min_diff == float('inf') else min_diff
+                    p += P[i-1]
+            
+            min_diff = min(min_diff, abs(total - 2*p))
+        
+    return min_diff if min_diff != float('inf') else -1
 
 def main():
     N = int(input())
