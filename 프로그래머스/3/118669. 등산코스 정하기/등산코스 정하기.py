77a98a8c344e89c306e46be5393789a39f953a5e
Nam Yuseong <dbtjd6511@gmail.com>
2026-04-29 17:00:30 +0900

    [level 3] Title: 등산코스 정하기, Time: 1001.69 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,50 +1,78 @@
-import heapq
+# n개의 지점
+# 1~n까지 번호
+
+# 출입구 / 쉼터 / 산봉우리
+# 양방향, 일정 시간 소요
+
+# 쉼터 / 산봉우리 > 휴식 가능
+# 휴식 없이 이동하는 시간 max = intensity
+
+# 출입구 > 산봉우리 > 원래 출입구
+# 출입구 처음/끝 1번씩 + 산봉우리 1번
+
+# 최소 intensity
+
+# 출입구에서 출발해서 산봉우리까지 가는데 > 어짜피 왔던길 돌아가면 됨
+
+# return [summit, min_intensity] > 여러 개라면 summit이 가장 작은 경우
+
+# n: 2~5만
+# paths: 5만 ~ 20만개
+    # [i, j, w]
+# gates: 1~5만
+# summits: 1~5만
+
+# 결론적으로 이동 비용이 최소인 값을 갱신하면서 등산
+    # 산봉우리 도달 > 후보에 추가
+
+# 다익스트라 > 등산 > multi_source로 gates에서 시작
+    # 특정 위치까지 갱신
+    # if cur_intensity > intensity:
+        # intensity = max(intensity, cur_intensity)
+    
+# O(ElogV) > O(20만 lg 5만)
+    
+import heapq    
 
 def solution(n, paths, gates, summits):
-        
-    # 1. graph 생성 - O(20만)
+    
     graph = {}
     for i, j, w in paths:
         graph.setdefault(i, []).append((j, w))
         graph.setdefault(j, []).append((i, w))
-        
-    # 2. 모든 gates에 대해서
-    
-    gates = set(gates)
-    summits = set(summits)
     
     INF = float('inf')
     
     pq = []
-    dist = [INF] * (n+1)
+    intensities = [INF] * (n+1)
     
-    result = []
-        
+    summits = set(summits)
+    candidates = []
+    
     for gate in gates:
         heapq.heappush(pq, (0, gate))
-        dist[gate] = 0
-    
+        intensities[gate] = 0
+        
     while pq:
-        intensity, cur_node = heapq.heappop(pq)
-            
-        if dist[cur_node] < intensity:
+        cur_intensity, cur_pos = heapq.heappop(pq)
+        
+        # 크면 다시 계산 필요 없음
+        if intensities[cur_pos] < cur_intensity:
             continue
-            
-        if cur_node in summits:
-            result.append([intensity, cur_node])        
+        
+        # 산봉우리면 탐색 종료
+        if cur_pos in summits:
+            candidates.append([cur_pos, cur_intensity])
             continue
+        
+        for pos, intensity in graph.get(cur_pos, []):
             
-        for node, c in graph.get(cur_node, []):
-            if node in gates:
-                continue
+            next_intensity = max(cur_intensity, intensity)
             
-            next_intensity = max(intensity, c)
-            
-            if next_intensity < dist[node]:
-                dist[node] = next_intensity
-                heapq.heappush(pq, (next_intensity, node))
-                
+            if next_intensity < intensities[pos]:
+                intensities[pos] = next_intensity
+                heapq.heappush(pq, (next_intensity, pos))
     
-    result.sort()
+    candidates.sort(key=lambda x: (x[1], x[0]))
     
-    return result[0][::-1]
+    return candidates[0]
