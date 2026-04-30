30574514c557214bd3d869a2d9b98a04dd63d128
Nam Yuseong <dbtjd6511@gmail.com>
2026-04-30 22:28:46 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 5.44 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.swift
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.swift
@@ -1,118 +1,61 @@
-import Foundation
-
-func solution(_ jobs: [[Int]]) -> Int {
-    var jobs = jobs.sorted { $0[0] < $1[0] } // 요청 시간 기준 정렬
-    var PQ = Heap<[Int]> { $0[1] < $1[1] } // 소요시간 기준 최소 힙
-    
-    var curT = 0 // 현재 시간
-    var returnTimes = 0 // 총 반환 시간
-    var idx = 0 // jobs 탐색 인덱스
-    let jobCount = jobs.count // 전체 작업 개수
-
-    while idx < jobCount || !PQ.isEmpty {
-        // 현재 시간(curT)까지 요청된 작업들을 힙에 추가
-        while idx < jobCount && jobs[idx][0] <= curT {
-            PQ.insert(jobs[idx])
-            idx += 1
-        }
-        
-        if !PQ.isEmpty {
-            let job = PQ.pop()!
-            curT += job[1] // 작업 수행
-            returnTimes += curT - job[0] // 요청부터 완료까지 걸린 시간
-        } else {
-            // 현재 진행할 작업이 없으면 다음 요청 시간으로 이동
-            curT = jobs[idx][0]
-        }
-    }
-    
-    return returnTimes / jobCount
-}
-
-// 우선순위 디스크 컨트롤러
-// 번호, 요청 시각, 소요시간
-// 가장 우선순위가 높은 작업
-// 1. 소요시간 짧은 것, 요청시각 빠른것, 번호가 작은것
-// 해당 작업만 수행
-
-// input: [ [요청시간, 소요시간] ]
-// 모든 요청 작업의 반환시간 평균
-// (반환시간 - 요청시간)의 평균
-
-// jobs: 1~500개
-// 요청시간: 0~1000
-// 소요시간: 1~1000
-
-// Heap을 구현, Compare: ([s,l], [s,l]) -> { if/else }
-// curT = 0
-// returnTimes = 0
-// 1. pop하고, O(lgN)
-// 시간 업데이트 curT = max(work[0], curT) + work[1]
-// returnTime += curT - work[0]
-
-// 2. 최종 returnTimes/jobs.count
-
 struct Heap<T> {
     var heap: [T] = []
-    var compare: (T, T) -> Bool
-    
-    var isEmpty: Bool {
-        heap.count < 2
+    var first: T? { 
+        if isEmpty { return nil }
+        return heap[1]
     }
-    
-    init(compare: @escaping (T,T) -> Bool) {
+    var isEmpty: Bool { heap.count < 2 }
+    var compare: (T,T) -> Bool
+    init(_ compare: @escaping (T, T) -> Bool) {
         self.compare = compare
     }
-    
-    mutating func insert(_ k: T) {
-        if heap.isEmpty {
-            heap.append(k)
-            heap.append(k)
+    mutating func heappush(_ item: T) {
+        if heap.isEmpty { 
+            heap.append(item)
+            heap.append(item)
             return
         }
-        
-        heap.append(k)
+        heap.append(item)
         swimUp(heap.count-1)
     }
-    
-    private mutating func swimUp(_ idx: Int) {
+    mutating func swimUp(_ idx: Int) {
         var curIdx = idx
-        
-        while curIdx > 1 && compare(heap[curIdx], heap[curIdx/2]) {
-            heap.swapAt(curIdx, curIdx/2)
+        while curIdx / 2 >= 1 && compare(heap[curIdx], heap[curIdx/2]) {
+            heap.swapAt(curIdx/2, curIdx)
             curIdx /= 2
         }
     }
     
-    mutating func pop() -> T? {
+    mutating func heappop() -> T? {
         if heap.count < 2 { return nil }
         if heap.count == 2 { return heap.popLast() }
         
+        let result = heap[1]
         heap.swapAt(1, heap.count-1)
-        let result = heap.removeLast()
+        _ = heap.popLast()
         swimDown(1)
         return result
     }
     
-    private mutating func swimDown(_ idx: Int) {
+    mutating func swimDown(_ idx: Int) {
         var curIdx = idx
         
         while true {
-            var swapIdx = curIdx
-            var left = curIdx * 2 
-            var right = curIdx * 2 + 1
+            var targetIdx = curIdx
+            let leftIdx = curIdx * 2
+            let rightIdx = curIdx * 2 + 1
             
-            if left < heap.count && compare(heap[left], heap[swapIdx]) {
-                swapIdx = left
+            if leftIdx < heap.count && compare(heap[leftIdx], heap[targetIdx]) {
+                targetIdx = leftIdx
             }
             
-            if right < heap.count && compare(heap[right], heap[swapIdx]) {
-                swapIdx = right
+            if rightIdx < heap.count && compare(heap[rightIdx], heap[targetIdx]) {
+                targetIdx = rightIdx
             }
             
-            if curIdx != swapIdx {
-                heap.swapAt(curIdx, swapIdx)
-                curIdx = swapIdx
+            if targetIdx != curIdx {
+                heap.swapAt(targetIdx, curIdx)
+                curIdx = targetIdx
             } else {
                 break
             }
@@ -121,18 +64,86 @@ struct Heap<T> {
 }
 
 
+import Foundation
+
+// 한번에 하나의 작업
+// 우선순위 디스크 컨트롤러
+
+// 대기큐 [작업번호, 요청시각, 소요시간]
+// 1. 우선순위가 높은 작업 > 하드디스크 진행
+//    소요시간 짧을 수록 > 요청시각 빠를 수록 > 작업번호가 작을 수록
+
+// 2. 한번에 1개의 작업만
+
+// 반환시간 = 작업요청 ~ 종료까지 걸린시간
+
+// [요청, 소요시간]
+
+// return avg(반환시간)
+
+// jobs: 500개
+// [요청 시각, 소요시간]
+
+// 1000초에 1000초 걸리는 게 500개
+// 최대 시간 50억초
 
 
+// 1. 요청 시각 순서대로 정렬
+// 2. 요청과 종료 시각마다 loop 연산
 
+// heap 사용
+    // 대기큐 : heap
+    // 
 
-
-
-
-
-
-
-
-
-
-
-
+func solution(_ jobs:[[Int]]) -> Int {
+    
+    var queue = Heap<(rqTime: Int, wkTime: Int, jobNo: Int)> { 
+        if $0.rqTime == $1.rqTime && $0.wkTime == $1.wkTime { return $0.jobNo < $1.jobNo }
+        return $0.wkTime == $1.wkTime ? $0.rqTime < $1.rqTime : $0.wkTime < $1.wkTime
+    }
+    
+    var hardDisk: [[Int]] = [] // (finish_time, rqTime, jobNo)
+    
+    var sortedJobs: [[Int]] = []
+    for (idx, job) in jobs.enumerated() { sortedJobs.append([job[0], job[1], idx]) }
+    sortedJobs.sort { $0[0] > $1[0] } // [요청 시각, 소요시간]
+    
+    var t = sortedJobs.last![0]
+    
+    var result: [Int] = []
+    
+    while !sortedJobs.isEmpty || !queue.isEmpty || !hardDisk.isEmpty {
+        
+        // 현재시각에 가능한 모든 작업 대기큐로 이동
+        while !sortedJobs.isEmpty && sortedJobs.last![0] <= t {
+            let job = sortedJobs.popLast()!
+            queue.heappush((rqTime: job[0], wkTime: job[1], jobNo: job[2]))
+        }
+        
+        if !hardDisk.isEmpty && hardDisk.last![0] <= t {
+            let job = hardDisk.popLast()!
+            result.append(job[0] - job[1])
+        }
+            
+        if hardDisk.isEmpty {
+            
+            if queue.isEmpty {
+                // 하드디스크가 가능 but 작업 없다면 > 작업 있는 시간으로 이동
+                if !sortedJobs.isEmpty {
+                    t = sortedJobs.last![0]
+                }
+                
+            } else {
+                // 하드디스크가 가능하면 > 작업 부여
+                let (rqTime, wkTime, jobNo) = queue.heappop()!
+                hardDisk.append([t+wkTime, rqTime, jobNo]) // (finish_time, rqTime, jobNo)  
+            }
+                    
+        } else {
+            // 하드디스크가 불가능 > 끝나는 시간으로 이동
+            t = hardDisk.last![0]
+        }
+    }
+    
+    return result.reduce(0, +) / result.count
+}
