63f4a220a9d396755daddfe4e1a6ad8121df1571
Nam Yuseong <dbtjd6511@gmail.com>
2026-08-25 14:19:13 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.61 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
@@ -1,68 +1,59 @@
 import Foundation
 
-// 개인 정보 - 약관 - 보관 유효기간
-// A 12달 - 2021 1월 5일 > 2022 1월 5일부터 파기
+// 29분~
 
-// 오늘 날짜로 파기해야할 개인 정보
+// 개인정보 n개
+// 유효기간
 
-// 모든 달은 28일까지
+// 파기해야할 개인 정보
+// 모든 달은 28일
+
+// 오늘
+
+// today: "YYYY.MM.DD"
+// terms: "A MONTH"
+// 
 
 
-// today 오늘
-// 약관의 유효기간 terms
-// 개인정보 privacies
+// 100개 돌면서 체킹해라
 
-// 파기해야할 개인정보 -> 오름차순으로 리턴
-
-// today "YYYY.MM.DD"
-// terms [ "A 1", "B 100" ] count: 1~20
-// privacies [ "YYYY.MM.DD A", "YYYY.MM.DD B" ], count: 1~100 today 이전 날짜만, terms 있는 약관만
-
-// YYYY 2000~2022
-// MM 01 ~ 12
-// DD 01 ~ 28
-
-// 파기 정보는 최소 1개 이상
-
-// 1. 날짜 -> 숫자로 변경
-// 2. 개월 * 28 더한 최종값 배열로 변경
-// 3. today보다 작은 값 filter
-// 4. sort
-
-func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
-    
-    // 오늘 날짜 -> 숫자로
-    let today = today.toDay()
-    
-    // 약관 개월 수로 변경
-    var TermMonth: [String: Int] = [:]
-    
-    for term in terms {
-        let t = term.split(separator: " ").map { String($0) }
-        TermMonth[t[0]] = Int(t[1])!
-    }
-    
-    return privacies
-        .map { $0.split(separator: " ").map { String($0) } }
-        .map { $0[0].toDay() + TermMonth[$0[1]]! * 28 }
-        .enumerated()
-        .filter { $0.1 <= today } // 오늘 날짜랑 같으면 만료된 것
-        .map { $0.0 + 1 }
-        .sorted()
-}
+// 1. 날짜를 일 단위로 변경
+// 2. 오늘 기준으로 빠졌나?
 
 extension String {
     func toDay() -> Int {
-        // "YYYY.MM.DD"
-        
-        let MONTH = 12
-        let DAY = 28
-        
-        let YMD = self.split(separator: ".").map { String($0) }
-        let Y = Int(YMD[0])! - 2000
-        let M = Int(YMD[1])! - 1
-        let D = Int(YMD[2])!
-        
-        return Y * MONTH * DAY + M * DAY + D
+        let date = self.split(separator: ".").map { Int(String($0))! }
+        let y = date[0]
+        let m = date[1]
+        let d = date[2]
+        return (y - 2000) * 12 * 28 + (m - 1) * 28 + d
     }
+}
+
+func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
+    
+    let today = today.toDay()
+    
+    var dict: [String: Int] = [:]
+    
+    for term in terms {
+        let t = term.split(separator: " ").map { String($0)}
+        dict[t[0]] = Int(t[1])! * 28
+    }
+    
+    var result: [Int] = []
+    
+    for (idx, privacy) in privacies.enumerated() {
+        
+        let p = privacy.split(separator: " ").map { String($0) }
+        
+        let day = p[0].toDay()
+        let expire = dict[p[1]]!
+        
+        if today >= day + expire {
+            result.append(idx+1)
+        }
+    }
+    
+    return result
 }
