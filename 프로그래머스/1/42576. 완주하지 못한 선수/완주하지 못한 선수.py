2d9bc1e33a3e8926c4559e3ea4d2414d064082f8
Nam Yuseong <dbtjd6511@gmail.com>
2026-08-26 08:53:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.10 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,13 @@
 def solution(participant, completion):
     
-    player = {}
+    participant.sort()
+    completion.sort()
     
-    for c in completion:
-        player[c] = player.get(c, 0) + 1    
+    while completion:
+        if participant[-1] != completion[-1]:
+            return participant[-1]
         
-    for p in participant:
-        if p in player and player[p] > 0:
-            player[p] -= 1
-        else:
-            return p
-
-# 마라톤 선수
-# 1명을 제외하고 완주
+        participant.pop()
+        completion.pop()
+    
+    return participant[-1]
