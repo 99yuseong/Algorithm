0d82ffe63592203bb29d19e98164152a6c6dfbb8
Nam Yuseong <dbtjd6511@gmail.com>
2026-08-26 09:01:45 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,3 @@
-# 다른 옷을 조합
-
-# 종류별 최대 1가지 의상
-# 최소 1개 이상의 의상
-# 서로 다른 조합의 수
-
-# 1개~30개 사이의 의상
-# Dict에 담고 > values * (v+1) ... -1 (선택 안 한 경우)
-
 def solution(clothes):
     
     d = {}
@@ -15,7 +6,7 @@ def solution(clothes):
         d[cloth[1]] = d.get(cloth[1], 0) + 1
         
     answer = 1
-    for v in d.values():
-        answer *= v+1
+    for k, v in d.items():
+        answer *= v + 1
         
     return answer - 1
