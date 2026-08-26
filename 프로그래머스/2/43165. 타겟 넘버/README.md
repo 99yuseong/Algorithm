cbf4418c94d35c41cdf704b85a9883116ba761bc
Nam Yuseong <dbtjd6511@gmail.com>
2026-08-26 10:19:35 +0900

    [level 2] Title: 타겟 넘버, Time: 28.17 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 9.09 MB, 시간: 413.42 ms
+메모리: 20.8 MB, 시간: 28.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 24일 10:50:39
+2026년 08월 26일 10:19:32
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -1,43 +1,18 @@
 import Foundation
 
-// n개의 음이 아닌 정수
-// 적절히 더하거나 빼서
-
-// 방법의 경우의 수를 구해라
-
-// 2~20개 숫자
-
-// 1~50사이
-// 타겟은 1~1000
-
-
-// 2^20 > 100만 경우의 수
-
-// 순서를 바꾸지 않고 - 더하거나 뺀다.
-
-
-
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     
-    var cnt = 0
-    var cur = 0
-    
-    func dfs(_ k: Int) {
-        
-        if k == numbers.count {
-            if target == cur { cnt += 1 }
+    var answer = 0
+    func dfs(_ num: Int, _ d: Int) {
+        if d == numbers.count {
+            if num == target { answer += 1 }
             return
         }
         
-        cur += numbers[k]
-        dfs(k+1)
-        cur -= numbers[k]
-        
-        cur -= numbers[k]
-        dfs(k+1)
-        cur += numbers[k]
-    }    
-    dfs(0)
+        dfs(num + numbers[d], d+1)
+        dfs(num - numbers[d], d+1)
+    }
+    dfs(0, 0)
     
-    return cnt
+    return answer
 }
