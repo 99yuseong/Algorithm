fdaa775b5e59f4cea893593c2fc53a41bcca1b75
Nam Yuseong <dbtjd6511@gmail.com>
2026-08-26 10:11:27 +0900

    [level 1] Title: 모의고사, Time: 1.22 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=swift) 
 
 ### 성능 요약
 
-메모리: 12 MB, 시간: 1.15 ms
+메모리: 21.2 MB, 시간: 1.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 26일 10:03:54
+2026년 08월 26일 10:11:25
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -1,30 +1,20 @@
-// 찍는다.
-// 1 - 12345
-// 2 - 21232425
-// 3 - 3311224455
-
-// N - 1만
-// 가장 많이 맞춘 사람을 배열에 리턴
-// 동점자는 정렬
-
 import Foundation
 
 func solution(_ answers:[Int]) -> [Int] {
     
-    let first = [1, 2, 3, 4, 5]
-    let second = [2, 1, 2, 3, 2, 4, 2, 5]
-    let third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    let p1 = [1, 2, 3, 4, 5]
+    let p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    let p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    var score = [0,0,0]
+    var score = [0, 0, 0]
     
-    // 계산
-    for (idx, answer) in answers.enumerated() {
-        score[0] += first[idx % first.count] == answer ? 1 : 0
-        score[1] += second[idx % second.count] == answer ? 1 : 0
-        score[2] += third[idx % third.count] == answer ? 1 : 0
+    for (i, answer) in answers.enumerated() {
+        if answer == p1[i % p1.count] { score[0] += 1 }
+        if answer == p2[i % p2.count] { score[1] += 1 }
+        if answer == p3[i % p3.count] { score[2] += 1 }
     }
     
-    let max = score.max()!
+    let m = score.max()!
     
-    return (1...3).filter { score[$0-1] == max }
+    return [1, 2, 3].filter { score[$0-1] == m }
 }
