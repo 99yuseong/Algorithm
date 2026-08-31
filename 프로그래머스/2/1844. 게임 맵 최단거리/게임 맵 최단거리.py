70d63bbdf477d87c94b2d1399978eaeba15559b9
Nam Yuseong <dbtjd6511@gmail.com>
2026-08-31 17:26:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.67 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,20 +1,8 @@
-# ROR 게임
-# 상대방 진영 파괴시 승리 > 최대한 빨리 도착해야한다.
-
-# 도착 가능할 수도, 못할 수도 있다.
-# 지나가야하는 칸의 최솟값 > 없으면 -1
-
-# BFS로 거리 카운트
-# 가중치가 1인 최단 경로 > BFS
-
-# 맵의 크기는 최대 1만
-# 벽: 0, 칸: 1
-
-# 1,1 > n,m으로
+# @time 13 
 
 from collections import deque
 
-def solution(maps):
+def bfs(maps):
     
     n = len(maps)
     m = len(maps[0])
@@ -22,21 +10,29 @@ def solution(maps):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
-    visited = [[-1] * m for _ in range(n)]
     queue = deque()
+    visited = [[-1 for _ in range(m)] for _ in range(n)]
     
     queue.append([0,0])
     visited[0][0] = 1
     
-    while len(queue) > 0:
+    while queue:
         cur = queue.popleft()
         
-        for k in range(4):
-            x = cur[0] + dx[k]
-            y = cur[1] + dy[k]
+        for i in range(4):
+            x = cur[0] + dx[i]
+            y = cur[1] + dy[i]
             
-            if 0 <= x < n and 0 <= y < m and maps[x][y] == 1 and visited[x][y] == -1:
+            if (
+                x >= 0 and x < n
+                and y >= 0 and y < m 
+                and visited[x][y] == -1 
+                and maps[x][y] == 1
+            ):
                 visited[x][y] = visited[cur[0]][cur[1]] + 1
                 queue.append([x, y])
-    
+                
     return visited[n-1][m-1]
+
+def solution(maps):
+    return bfs(maps)
