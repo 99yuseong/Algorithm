ec2445c504a43df8fb17249ae494f074a9a77075
Nam Yuseong <dbtjd6511@gmail.com>
2026-08-31 18:36:19 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,32 +1,30 @@
-# 모든 티켓을 다써서 여행 경로, 불가능한 경우는 주어지지 않는다. = 타캣을 다 쓰면 다 도착한다는 개념
-# ICN에서 출발해서 티켓을 다쓰는 경로 중 알파벳 순서가 앞서는 경우가 무엇인가.
+# @time -1
+# @tags DFS
 
-# 최대 경로는 10000개 티켓
-
-# 그리디로 정렬하고
-# dfs로 가장 먼저 도착하는 경우를 return
+from collections import defaultdict
 
 def solution(tickets):
     
-    # 도착 경로를 알파벳 순으로 정렬
-    tickets.sort(key=lambda x: x[1])
-    
     n = len(tickets)
-    used = [False] * n
     
-    def dfs(route):
-        nonlocal used
-        
-        if len(route) == n+1:
-            return route
-        
-        for i in range(n):
-            if used[i] is False and tickets[i][0] == route[-1]:
-                used[i] = True
-                rt = dfs(route + [tickets[i][1]])
-                used[i] = False
-                
-                if rt:
-                    return rt
+    tickets.sort(key=lambda x: (x[0], x[1]))
     
-    return dfs(["ICN"])
+    graph = defaultdict(list)
+    
+    for start, end in tickets:
+        graph[start].append(end)
+        
+    for start in graph:
+        graph[start].sort(reverse=True)
+    
+    route = []
+    
+    def dfs(airport):
+        while graph[airport]:
+            next_airport = graph[airport].pop()
+            dfs(next_airport)
+        route.append(airport)
+    
+    dfs("ICN")
+    
+    return route[::-1]
