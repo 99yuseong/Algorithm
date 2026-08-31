50720b61c28018cc67f2ae1da3bc4c4c7de64560
Nam Yuseong <dbtjd6511@gmail.com>
2026-08-31 18:17:33 +0900

    [level 3] Title: 단어 변환, Time: 0.83 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,73 +1,47 @@
-# begin, target
-# begin > target
+# @time -1
+# @tags BFS
 
-# 1개의 알파벳 변경
-# words에 있는 단어로만 변환
+from collections import deque, defaultdict
 
-# 최소 몇단계로 변환? 불가능시 0
-
-# 길이는 10 > 
-# 50개의 단어 - 최대 50번 변경
-
-# 0. target이 words안에 있는가? 확인 > 0
-
-
-# 1. 문자열이 1개만 다른거를 매핑 [graph]
-# 2. BFS로 변경하면서 visited + 1
-# 3. 최종 target 도착시 리턴, 아니라면 0
-
-from collections import deque
+def diff(w1, w2):
+    diff = 0
+    for i in range(len(w1)):
+        if w1[i] != w2[i]:
+            diff += 1
+    return diff
 
 def solution(begin, target, words):
     
-    if target not in words:
-        return 0
+    n = len(words)
+    graph = defaultdict(list)
     
-    # graph 매핑
+    for i in range(n):
+        for j in range(n):
+            w1, w2 = words[i], words[j]
+            
+            dif = diff(w1, w2)
+            
+            if dif == 1:
+                graph[i].append(j)
+                graph[j].append(i)
     
-    graph = {}
-    for word1 in words:
-        for word2 in words:
-            if word1 == word2:
-                continue
-                
-            diff = 0
-            for i in range(len(word1)):
-                if diff > 1: break
-                if word1[i] != word2[i]: 
-                    diff += 1
-        
-            if diff == 1:
-                graph.setdefault(word1, []).append(word2)
-    
-    visited = {}
-    for word in words:
-        visited.setdefault(word, -1)
     queue = deque()
+    visited = [-1 for _ in range(n)]
     
-    for i in range(len(words)):
-        word = words[i]
-        diff = 0
-        
-        for j in range(len(word)):
-            if diff > 1: break
-            if begin[j] != word[j]:
-                diff += 1
-        
-        if diff == 1:
-            queue.append(word)
-            visited[word] = 1
-    
-    
-    while len(queue) > 0:
+    for i in range(n):
+        if diff(begin, words[i]) == 1:
+            queue.append(i)
+            visited[i] = 1
+            
+    while queue:
         cur = queue.popleft()
-        
-        if cur == target:
+                
+        if words[cur] == target:
             return visited[cur]
-        
-        for v in graph.get(cur, []):
-            if visited[v] == -1:
-                visited[v] = visited[cur] + 1
-                queue.append(v)
-    
+                
+        for next in graph[cur]:
+            if diff(words[cur], words[next]) == 1 and visited[next] == -1:
+                queue.append(next)
+                visited[next] = visited[cur] + 1
+            
     return 0
