ae55f6f3ebf6429314a12b1ca6a80a0958bab6cf
Nam Yuseong <dbtjd6511@gmail.com>
2026-09-02 11:31:42 +0900

    [level 3] Title: 징검다리 건너기, Time: 330.43 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,28 +1,29 @@
-# 35 ~ @time -1
+# @time -1
 
-def canCross(stones, k, x):
-    continuous = 0
+
+def cal_jump_dist(stones, crossed):
     
-    for stone in stones:
-        if stone - x <= 0:
-            continuous += 1
+    max_jump_dist = 0
+    jump_dist = 0
+    
+    for i in range(len(stones)):
+        if stones[i] <= crossed:
+            jump_dist += 1
         else:
-            continuous = 0
-        
-        if continuous >= k:
-            return False
-        
-    return True
+            max_jump_dist = max(max_jump_dist, jump_dist)
+            jump_dist = 0
+    
+    return max(max_jump_dist, jump_dist)     
 
 def solution(stones, k):
     
-    st = 1
+    st = 1 
     en = max(stones)
     
     while st < en:
         mid = (st + en) // 2
         
-        if canCross(stones, k, mid):
+        if cal_jump_dist(stones, mid) < k:
             st = mid + 1
         else:
             en = mid
