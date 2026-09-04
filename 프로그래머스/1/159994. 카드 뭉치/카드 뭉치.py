8fdf7e4ee021a299e4db425c40ca47d204dbdb50
Nam Yuseong <dbtjd6511@gmail.com>
2026-09-04 11:45:45 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,18 @@
+# @time 5
+
+def solution(cards1, cards2, goal):
+    
+    # stack으로 전환
+    cards1.reverse()
+    cards2.reverse()
+    
+    for word in goal:
+        
+        if cards1 and cards1[-1] == word:
+            cards1.pop()
+        elif cards2 and cards2[-1] == word:
+            cards2.pop()
+        else:
+            return "No"
+    
+    return "Yes"
