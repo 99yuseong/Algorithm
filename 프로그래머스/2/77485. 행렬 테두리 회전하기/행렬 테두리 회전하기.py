a3681130365e20850a232947ff30bc8573af471c
Nam Yuseong <dbtjd6511@gmail.com>
2026-09-05 17:40:50 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 56.56 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,68 +1,51 @@
-# 범위를 선택 > 시계방향 회전
-# 회전 목록의 최소값을 저장
-# return [회전1 최솟값, 회전2 최솟값 ... ]
+# @time 15
+# @tags 구현
 
-# rows: 2~100
-# cols: 2~100
-
-# 최대 100칸 > 최대 40칸의 원소를 옮긴다.
-# 회전 횟수는 1~1만개
-
-# (40칸 원소 회전 + 40칸 원소 갱신) * 1만번 회전
-
-def rotate(map, x1, y1, x2, y2):
+def rotate(box, x1, y1, x2, y2):
     
-    if x1+1 == x2 and y1+1 == y2:
-        tmp = map[x1][y1]
-        map[x1][y1] = map[x2][y1]
-        map[x2][y1] = map[x2][y2]
-        map[x2][y2] = map[x1][y2]
-        map[x1][y2] = tmp
-        return min(map[x1][y1], map[x2][y1], map[x2][y2], map[x1][y2])
-    
-    topleft = map[x1][y1]
-    topright = map[x1][y2]
-    bottomleft = map[x2][y1]
-    bottomright = map[x2][y2]
-    
-    min_num = min([topleft, topright, bottomleft, bottomright])
+    start = box[x1][y1]
+    min_num = start
     
     for i in range(x1, x2):
-        min_num = min(min_num, map[i+1][y1])
-        map[i][y1] = map[i+1][y1]
+        box[i][y1] = box[i+1][y1]
+        min_num = min(min_num, box[i+1][y1])
         
-    for j in range(y2, y1+1, -1):
-        min_num = min(min_num, map[x1][j-1])
-        map[x1][j] = map[x1][j-1]
-    map[x1][y1+1] = topleft
+    for j in range(y1, y2):
+        box[x2][j] = box[x2][j+1]
+        min_num = min(min_num, box[x2][j+1])
     
-    for i in range(x2, x1+1, -1):
-        min_num = min(min_num, map[i-1][y2])
-        map[i][y2] = map[i-1][y2]
-    map[x1+1][y2] = topright
+    for i in range(x2, x1, -1):
+        box[i][y2] = box[i-1][y2]
+        min_num = min(min_num, box[i-1][y2])
+        
+    for j in range(y2, y1, -1):
+        box[x1][j] = box[x1][j-1]
+        min_num = min(min_num, box[x1][j-1])
+        
+    box[x1][y1+1] = start
     
-    for j in range(y1, y2-1):
-        min_num = min(min_num, map[x2][j+1])
-        map[x2][j] = map[x2][j+1]
-    map[x2][y2-1] = bottomright
     
     return min_num
 
 def solution(rows, columns, queries):
     
+    box = [[0] * columns for _ in range(rows)]
     
-    map = [[0] * columns for _ in range(rows)]
+    n = 1
     
-    num = 1
     for i in range(rows):
         for j in range(columns):
-            map[i][j] = num
-            num += 1
-    
+            box[i][j] = n
+            n += 1
+            
     answer = []
     
-    for x1, y1, x2, y2 in queries:
-        min_num = rotate(map, x1-1, y1-1, x2-1, y2-1)
+    for query in queries:
+        
+        x1, y1, x2, y2 = query
+        
+        min_num = rotate(box, x1-1, y1-1, x2-1, y2-1)
+    
         answer.append(min_num)
     
     return answer
