92de01678afad6a513492eacc765972af4b22a5b
Nam Yuseong <dbtjd6511@gmail.com>
2026-09-05 18:35:17 +0900

    [level 3] Title: 이중우선순위큐, Time: 45.32 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
 
 ### 성능 요약
 
-메모리: 23.1 MB, 시간: 94.84 ms
+메모리: 20.8 MB, 시간: 45.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 31일 10:27:19
+2026년 09월 05일 18:35:14
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,47 +1,56 @@
-# 이중 우선순위 큐
-# 모든 연산 후 큐가 비어있으면 [0,0] 아니면 [최대, 최소]
-
-# 100만 cmd
-# 빈큐 삭제 명령은 무시
-
-# 최대힙, 최소힙 따로?
-# 숫자는 중복될 수 있다. 빠짐 체크가 애매하긴함. > id를 사용하자
+# @time 19
+# @tags Heap
 
 import heapq
+from collections import defaultdict
 
 def solution(operations):
     
+    d = defaultdict(int)
+    
     min_heap = []
     max_heap = []
-    removed_set = set()
     
-    item_id = 1
     for op in operations:
-        cmd, num = op.split()
-        num = int(num)
+        o, n = op.split()
         
-        if cmd == 'I': # 큐에 추가
-            heapq.heappush(min_heap, (num, item_id))
-            heapq.heappush(max_heap, (-num, item_id))
-            item_id += 1
+        if o == "I":
+            heapq.heappush(min_heap, int(n))
+            heapq.heappush(max_heap, -int(n))
+            d[int(n)] += 1
         
-        elif num == 1: # 최댓값 삭제
-            while max_heap and max_heap[0][1] in removed_set:
+        elif int(n) == 1:
+            
+            # 최댓힙 정리
+            while max_heap and d[-max_heap[0]] == 0:
                 heapq.heappop(max_heap)
-            if max_heap:
-                removed_set.add(heapq.heappop(max_heap)[1])
+            
+            if max_heap and d[-max_heap[0]] > 0:
+                d[-max_heap[0]] -= 1
+                heapq.heappop(max_heap)
         
-        else: # 최솟값 삭제
-            while min_heap and min_heap[0][1] in removed_set:
+        else:
+            # 최솟값 제거
+            while min_heap and d[min_heap[0]] == 0:
                 heapq.heappop(min_heap)
-            if min_heap:
-                removed_set.add(heapq.heappop(min_heap)[1])
-        
+            
+            if min_heap and d[min_heap[0]] > 0:
+                d[min_heap[0]] -= 1
+                heapq.heappop(min_heap)
     
-    while max_heap and max_heap[0][1] in removed_set:
-        heapq.heappop(max_heap)
+    left = 0
     
-    while min_heap and min_heap[0][1] in removed_set:
-        heapq.heappop(min_heap)
+    for k in d:
+        if d[k] > 0:
+            left += 1
     
-    return [-max_heap[0][0], min_heap[0][0]] if min_heap and max_heap else [0,0]
+    if left == 0:
+        return [0,0]
+    
+    else:
+        while min_heap and d[min_heap[0]] == 0:
+            heapq.heappop(min_heap)
+        while max_heap and d[-max_heap[0]] == 0:
+            heapq.heappop(max_heap)
+    
+        return [-max_heap[0], min_heap[0]]
