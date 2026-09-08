61ce4f69414360c0c2802bdfc1700c2256de2808
Nam Yuseong <dbtjd6511@gmail.com>
2026-09-08 11:07:01 +0900

    [level 3] Title: 정수 삼각형, Time: 24.85 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,30 +1,24 @@
-# 꼭대기 > 바닥
+# @time 30
+# @tags DP
+
 # 거쳐간 숫자의 합이 가장 큰 경우
 
+# DP[i][j] : i번째에서 j인덱스를 선택했을 떄 최댓값
 
-# [a, b, c, d, e] > 0 > 
-# 왼쪽: 2*i + 1, 오른쪽: 2*i + 2
+# DP[i][j] = max(DP[i-1][j-1], DP[i-1][j]) + triangle[i][j]
 
-# 매번 좌,우를 선택
-# 모든 경우의 수를 관리 > 500회의 선택
-# 2^500 > 시간초과
-
-# DP로 저장
-# DP[i][k]: i번째 층에서 k번째 길을 선택했을때 최댓값
-# DP[i][k] = max(DP[i-1][k-1], DP[i-1][k]) + A[i-1][k-1]
-# DP[1][1] = A[0][0]
-# 1 + ... + 500 = 약 1만 2500
+# DP[0][0] = triangle[0][0]
 
 def solution(triangle):
     
     n = len(triangle)
-    k = len(triangle[-1])
     
-    DP = [[0] * (k+1) for _ in range(n+1)]
-    DP[1][1] = triangle[1-1][1-1]
+    DP = [[0] * (n+1) for _ in range(n+1)]
     
-    for i in range(2, n+1): # 2층부터 n층
-        for j in range(1, i+1): # 1번부터 i번까지 길
-            DP[i][j] = max(DP[i-1][j-1], DP[i-1][j]) + triangle[i-1][j-1]
+    DP[0][0] = triangle[0][0]
     
-    return max(DP[-1])
+    for i in range(1, n):
+        for j in range(i+1):
+            DP[i][j] = max(DP[i-1][j-1], DP[i-1][j]) + triangle[i][j]
+        
+    return max(DP[n-1])
